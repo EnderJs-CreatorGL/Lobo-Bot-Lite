1f1b2f09ce270a82da92c80afe0440835326869d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-09-18 16:48:49 -0600

    Hachiko-Bot-MD

---
+++ plugins/rpg-levelup.js
@@ -0,0 +1,45 @@
+import {canLevelUp, xpRange} from '../lib/levelling.js';
+import {levelup} from '../lib/canvas.js';
+
+const handler = async (m, {conn}) => {
+  const name = conn.getName(m.sender);
+  const user = global.db.data.users[m.sender];
+  if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+    const {min, xp, max} = xpRange(user.level, global.multiplier);
+    throw `
+┌───⊷ *NIVEL*
+▢ Nombre : *${name}*
+▢ Nivel : *${user.level}*
+▢ XP : *${user.exp - min}/${xp}*
+└──────────────
+
+Te falta *${max - user.exp}* de *XP* para subir de nivel
+`.trim();
+  }
+  const before = user.level * 1;
+  while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++;
+  if (before !== user.level) {
+    const teks = `🎊 Bien hecho ${conn.getName(m.sender)}    Nivel:`;
+    const str = `
+┌─⊷ *LEVEL UP*
+▢ Nivel anterior : *${before}*
+▢ Nivel actual : *${user.level}*
+└──────────────
+
+*_Cuanto más interactúes con los bots, mayor será tu nivel_*
+`.trim();
+    try {
+      const img = await levelup(teks, user.level);
+      conn.sendFile(m.chat, img, 'levelup.jpg', str, m);
+    } catch (e) {
+      m.reply(str);
+    }
+  }
+};
+
+handler.help = ['levelup'];
+handler.tags = ['xp'];
+
+handler.command = ['nivel', 'lvl', 'levelup', 'level'];
+
+export default handler;
