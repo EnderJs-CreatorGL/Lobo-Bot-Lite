2156f982839e3c0bb4166b51adde6af80d2ce2aa
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-22 13:31:04 -0600

    Update _autosticker.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -16,8 +16,8 @@ handler.all = async function(m) {
       if (!img) return;
       stiker = await sticker(img, false, packname, author);
     } else if (/video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 7 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*\n\n𝙿𝙰𝚁𝙰 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙴𝚂𝚃𝙰 𝙾𝙿𝙲𝙸𝙾́𝙽 𝙴𝚂𝙲𝚁𝙸𝙱𝙰 (#𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚊𝚞𝚝𝚘𝚜𝚝𝚒𝚌𝚔𝚎𝚛)`);
-      // await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 7 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply(`[❗] 𝐍𝐨 𝐦𝐚𝐬 𝐝𝐞 7 𝐬𝐞𝐠𝐮𝐧𝐝𝐨𝐬 𝐞𝐥 𝐯𝐢𝐝𝐞𝐨*\n\n𝐩𝐚𝐫𝐚 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐫 𝐞𝐬𝐜𝐫𝐢𝐛𝐚 (#𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚊𝚞𝚝𝚘𝚜𝚝𝚒𝚌𝚔𝚎𝚛)`);
+      // await this.sendButton(m.chat, '[❗] 𝐍𝐨 𝐦𝐚𝐬 𝐝𝐞 7 𝐬𝐞𝐠𝐮𝐧𝐝𝐨𝐬 𝐞𝐥 𝐯𝐢𝐝𝐞𝐨', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
       const img = await q.download();
       if (!img) return;
       stiker = await sticker(img, false, packname, author);
