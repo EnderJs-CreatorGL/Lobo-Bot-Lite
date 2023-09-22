730c2620dfa718c8381ab560e8ee5bb83f4dc537
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-22 17:02:24 -0600

    Update afk-afk.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ const handler = async (m, {text}) => {
   const user = global.db.data.users[m.sender];
   user.afk = + new Date;
   user.afkReason = text;
-  m.reply(`[🚫] 𝐄𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 ${conn.getName(m.sender)} 𝐄𝐬𝐭𝐚𝐫𝐚 𝐎𝐟𝐥𝐢𝐧𝐞 (𝙰𝙵𝙺), 𝐍𝐨 𝐥𝐨 𝐞𝐭𝐢𝐪𝐮𝐞𝐭𝐞𝐧\n\n—◉ 𝐌𝐨𝐭𝐢𝐯𝐨 𝐨𝐟𝐥𝐢𝐧𝐞 (𝙰𝙵𝙺)${text ? ': ' + text : ''}*
+  m.reply(`[🚫] 𝐄𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 ${conn.getName(m.sender)} 𝐄𝐬𝐭𝐚𝐫𝐚 𝐎𝐟𝐥𝐢𝐧𝐞 (𝙰𝙵𝙺), 𝐍𝐨 𝐥𝐨 𝐞𝐭𝐢𝐪𝐮𝐞𝐭𝐞𝐧\n\n—◉ 𝐌𝐨𝐭𝐢𝐯𝐨 𝐨𝐟𝐥𝐢𝐧𝐞 (𝙰𝙵𝙺)${text ? ': ' + text : ''}
 `);
 };
 handler.help = ['afk [alasan]'];
