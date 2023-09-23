8c637c0dc72fe8ac115c44e254c668ac56981ad8
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-23 17:49:02 -0600

    Update info-grouplist.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn}) => {
   let txt = '';
   for (const [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '𝙽𝙾 𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴𝚂' : '𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴'}]\n\n`;
-  m.reply(`*𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙴𝙽 𝙴𝙻 𝚀𝚄𝙴 𝙴𝚂𝚃𝙰 𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃:*
+  m.reply(`𝐆𝐑𝐔𝐏𝐎𝐒 𝐃𝐎𝐍𝐃𝐄 𝐄𝐒𝐓𝐀 𝐋𝐎𝐁𝐎-𝐁𝐎𝐓-𝐌𝐃:
 ${txt}
 `.trim());
 };
