e3fad38f385a98d286999f38a8887b68f5cda290
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-23 17:39:58 -0600

    Update buscador-stickersearch.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/buscador-stickersearch.js
+++ plugins/buscador-stickersearch.js
@@ -6,13 +6,13 @@ const handler = async (m, {text, usedPrefix, command, conn}) => {
   try {
     const res2 = await googleImage(text);
     const sfoto = res2.getRandom();
-    if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*`;
+    if (!text) throw `[❗] 𝐈𝐧𝐠𝐫𝐞𝐬𝐚 𝐞𝐥 𝐧𝐨𝐦𝐛𝐫𝐞 𝐝𝐞𝐥 𝐩𝐚𝐪𝐮𝐞𝐭𝐞 𝐚 𝐛𝐮𝐬𝐜𝐚𝐫`;
     const json = await fetch(`https://api.akuari.my.id/search/sticker?query=${text}`);
     const jsons = await json.json();
     const res = jsons.result.map((v, index) => `*🪴 • Resultado:* ${1 + index}\n*🌵 • Nombre:* ${v.title}\n*🍂 • Url:* ${v.url}`).join`\n\n───\n\n`;
     await conn.sendFile(m.chat, sfoto, 'error.jpg', res, m);
   } catch {
-    await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
+    await m.reply('[❗] 𝐅𝐚𝐥𝐥𝐨, 𝐢𝐧𝐭𝐞𝐧𝐭𝐞 𝐝𝐞 𝐧𝐮𝐞𝐯𝐨');
   }
 };
 handler.tags = ['sticker', 'search'];
