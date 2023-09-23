9d636bf9245b5ea10bb138d6aab2450f05035829
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-22 21:26:52 -0600

    Update _antitoxic.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `𝐇𝐨𝐥𝐚 *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, 𝐝𝐞𝐜𝐢𝐫 𝐥𝐚 𝐩𝐚𝐥𝐚𝐛𝐫𝐚 (${isToxic}) 𝐞𝐬𝐭𝐚 𝐩𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐚 *${user.warn}/5* 𝐚𝐝𝐯𝐞𝐫𝐭𝐞𝐧𝐜𝐢𝐚 [💢]`, false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`Hola *@${m.sender.split`@`[0]}*, superaste las 5 advertencias serás bloqueado y eliminado de este grupo`, false, {mentions: [m.sender]});
+    await m.reply(`𝐌𝐦𝐦 *@${m.sender.split`@`[0]}*, 𝐬𝐮𝐩𝐞𝐫𝐚𝐬𝐭𝐞𝐬 𝐥𝐚𝐬 𝐚𝐝𝐯𝐞𝐫𝐭𝐞𝐧𝐜𝐢𝐚𝐬 𝐬𝐞𝐫𝐚𝐬 𝐯𝐞𝐭𝐚𝐝𝐨 𝐝𝐞 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 [💢]`, false, {mentions: [m.sender]});
     user.banned = true;
     await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
