b887387e7aa7ef4334518fa94bf6cb91cf9b1272
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-10 08:34:39 -0600

    Update config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, isROwner}) => {
   const optionsFull =`┏━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ ◉— ✦🅻🅾🅱🅾-🅑🅞🅣-🅼🅳✦ —◉
+┃ ◉— ✦🅻OBO-BOT🅣-M🅳✦ —◉
 ┃✦≡≡≡≡≡≡≡≡≡≡≡✦≡≡≡≡≡≡≡≡≡≡≡≡✦
 ┃☆𝐏𝐀𝐍𝐄𝐋 𝐄𝐍𝐀𝐁𝐋𝐄 𝐘 𝐃𝐈𝐒𝐀𝐁𝐋𝐄☆ 
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛
@@ -8,249 +8,249 @@ const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, is
 
  ✦𝗠𝗢𝗗𝗢: 🌟☆||☆𝐖𝐄𝐋𝐂𝐎𝐌𝐄
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} welcome
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} welcome
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva la bienvenida en el grupo.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva la bienvenida en el grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 👥☆||☆𝐌𝐎𝐃𝐎 𝐏𝐔𝐁𝐋𝐈𝐂𝐎 
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} public
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} public
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El bot se vuelve de uso publico y/o privado.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El bot se vuelve de uso publico y/o privado*
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podra ser usado por owners del bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podra ser usado por owners del bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗠𝗢𝗗𝗢: 🔥☆||☆𝐌𝐎𝐃𝐎 𝐇𝐎𝐑𝐍𝐘
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} modohorny
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} modohorny
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva los comandos +18 en el grupo.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva los comandos +18 en el grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🚨☆||☆𝐀𝐍𝐓𝐈𝐋𝐈𝐍𝐊
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antilink
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antilink
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva el anti-enlaces de WhatsApp.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva el anti-enlaces de WhatsApp*
 
-*✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict.
+✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🚨☆||☆𝐀𝐍𝐓𝐈𝐋𝐈𝐍𝐊2
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antilink2
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antilink2
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva el anti-enlaces que inician en HTTPS.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva el anti-enlaces que inician en HTTPS*
 
-*✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict.
+✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🔎☆||☆𝐃𝐄𝐓𝐄𝐂𝐓
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} detect
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} detect
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desacriva las notificaciones de cambios en el grupo.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desacriva las notificaciones de cambios en el grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
 
  ✦𝗠𝗢𝗗𝗢: 🔎☆||☆𝐃𝐄𝐓𝐄𝐂𝐓 2
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} detect2
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} detect2
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Detecta modificaciones en el grupo y mantiene una mejor gestion.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Detecta modificaciones en el grupo y mantiene una mejor gestion*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 🛡☆||☆𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} restrict
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} restrict
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva las restricciones del Bot, como la de sacar o agregar personas a un grupo.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva las restricciones del Bot, como la de sacar o agregar personas a un grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podra ser usado por owners del bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podra ser usado por owners del bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 🔮☆||☆𝐀𝐔𝐓𝐎𝐑𝐄𝐀𝐃
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} autoread
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} autoread
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Marca como leido los mensajes Pestados automáticamente.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Marca como leido los mensajes Pestados automáticamente*
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗠𝗢𝗗𝗢: 🔊☆||☆𝐀𝐔𝐃𝐈𝐎𝐒
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} audios
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} audios
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva los comandos de audios sin prefijos, en el grupo.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva los comandos de audios sin prefijos, en el grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗠𝗢𝗗𝗢: 🎨☆||☆𝐀𝐔𝐓𝐎𝐒𝐓𝐈𝐂𝐊𝐄𝐑
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} autosticker 
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} autosticker 
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Todas las imagenes o videos enviados en el grupo se convierten en stickers. 
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Todas las imagenes o videos enviados en el grupo se convierten en stickers*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 💬☆||☆𝐏𝐂𝐎𝐍𝐋𝐘
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} pconly
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} pconly
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot solo responderá a los comandos si es ANTILLAMado.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot solo responderá a los comandos si es ANTILLAMado*
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 🗺️☆||☆𝐆𝐂𝐎𝐍𝐋𝐘
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} gconly
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} gconly
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot solo respondera a los comandos si es un grupo. 
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot solo respondera a los comandos si es un grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Esta comando solo podrá ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Esta comando solo podrá ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🖼️☆||☆𝐀𝐍𝐓𝐈𝐕𝐈𝐄𝐖𝐎𝐍𝐂𝐄 
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antiviewonce
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antiviewonce
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Las imagenes enviadas para ver solo una vez, son reenviadas normal por el Bot. 
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Las imagenes enviadas para ver solo una vez, son reenviadas normal por el Bot*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 📵☆||☆𝐀𝐍𝐓𝐈 𝐋𝐋𝐀𝐌𝐀𝐃𝐀
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} anticall
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} anticall
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot bloquerá a las personas que llamen al Bot. 
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot bloquerá a las personas que llamen al Bot* 
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podra ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podra ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 🚫☆||☆𝐀𝐍𝐓𝐈𝐏𝐑𝐈𝐕𝐀𝐃𝐎
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antiprivado
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antiprivado
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot bloquerá a las personas que escriban al privado del Bot. 
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot bloquerá a las personas que escriban al privado del Bot* 
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🤬☆||☆𝐀𝐍𝐓𝐈𝐓𝐎𝐗𝐈𝐂
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antitoxic
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antitoxic
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Detecta la malas palabras y advierte al participante del grupo, antes de ser eliminado.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Detecta la malas palabras y advierte al participante del grupo, antes de ser eliminado*
 
-*✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict.
+✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🖇️☆||☆𝐀𝐍𝐓𝐈𝐓𝐑𝐀𝐁𝐀𝐒
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antitraba
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antitraba
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*  El Bot detecta textos largos que podrian ser virus y causar lag en el chat y elimina al usuario.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*  El Bot detecta textos largos que podrian ser virus y causar lag en el chat y elimina al usuario*
 
-*✓𝗡𝗢𝗧𝗔:*  Se necesita tener activo el restrict.
+✓𝗡𝗢𝗧𝗔:*  Se necesita tener activo el restrict*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗠𝗢𝗗𝗢: 🔪☆||☆𝐀𝐍𝐓𝐈𝐀𝐑𝐀𝐁𝐄𝐒
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antiarabes
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antiarabes
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Si un numero árabe se uné al grupo, el Bot lo elimina automaticamente.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Si un numero árabe se uné al grupo, el Bot lo elimina automaticamente*
 
-*✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el welcome y el restrict.
+✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el welcome y el restrict*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🔪☆||☆𝐀𝐍𝐓𝐈𝐀𝐑𝐀𝐁𝐄𝐒 2
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antiarabes2
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antiarabes2
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Si un numero árabe escribe en el grupo, el Bot lo elimina automaticamente.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Si un numero árabe escribe en el grupo, el Bot lo elimina automaticamente*
 
-*✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict.
+✓𝗡𝗢𝗧𝗔:*Se necesita tener activo el restrict*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 🤖☆||☆𝐌𝐎𝐃𝐄𝐉𝐀𝐃𝐈𝐁𝐎𝐓
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} modejadibot
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} modejadibot
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva el uso del comando para sub bots (${usedPrefix}serbot / ${usedPrefix}jadibot).
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Activa o desactiva el uso del comando para sub bots (${usedPrefix}serbot / ${usedPrefix}jadibot)*
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 💻☆||☆𝐌𝐎𝐃𝐎𝐀𝐃𝐌𝐈𝐍
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} modoadmin
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} modoadmin
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot solo responderá a los admins del grupo.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot solo responderá a los admins del grupo*
 
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗠𝗢𝗗𝗢: 🛰️☆||☆𝐒𝐈𝐌𝐒𝐈𝐌𝐈
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} simsimi
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} simsimi
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot empezará a responder a los mensajes usando la IA de SimSimi.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:El Bot empezará a responder a los mensajes usando la IA de SimSimi*
 
-*✓𝗡𝗢𝗧𝗔:*Comando Solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:Comando Solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: ⏳☆||☆𝐀𝐍𝐓𝐈𝐒𝐏𝐀𝐌
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antispam
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antispam
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot detecta cuando un usuario hace spam de comando y lo banea por 5 segundos y lo advierte.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot detecta cuando un usuario hace spam de comando y lo banea por 5 segundos y lo advierte*
 
-*✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot.
+✓𝗡𝗢𝗧𝗔:*Este comando solo podrá ser usado por owners del Bot*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
  ✦𝗠𝗢𝗗𝗢: 🧿☆||☆𝐀𝐍𝐓𝐈𝐃𝐄𝐋𝐄𝐓𝐄 
+ 
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} antidelete
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} antidelete
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot detecta cuando un usuario elimina un mensaje y lo reenvía*
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*El Bot detecta cuando un usuario elimina un mensaje y lo reenvía.
-
-*✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo.
+✓𝗡𝗢𝗧𝗔:*Comando solo para admins de grupo*
 ⧫≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋⧫
 
 
  ✦𝗟𝗢𝗕𝗢-𝗕𝗢𝗧: 🔊☆||☆𝐀𝐔𝐃𝐈𝐎𝐒_𝐁𝐎𝐓
 
-*✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢:* ${usedPrefix + command} audios_bot
+✓𝗖𝗢𝗠𝗔𝗡𝗗𝗢: ${usedPrefix + command} audios_bot
 
-*✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Se desactivan los audios del Bot del menuaudios para todos los chats privados.
+✓𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗖𝗜𝗢𝗡:*Se desactivan los audios del Bot del menuaudios para todos los chats privados*
 
-*✓𝗡𝗢𝗧𝗔:* Este comando solo podrá ser usado por owners del Bot.`.trim();
+✓𝗡𝗢𝗧𝗔:* Este comando solo podrá ser usado por owners del Bot*`.trim();
 
   const isEnable = /true|enable|(turn)?on|1/i.test(command);
   const chat = global.db.data.chats[m.chat];
@@ -534,7 +534,7 @@ const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, is
       if (!/[01]/.test(command)) return await conn.sendMessage(m.chat, {text: optionsFull}, {quoted: m});
       throw false;
   }
-  conn.sendMessage(m.chat, {text: `📃 𝗢𝗣𝗖𝗜𝗢𝗡: ${type}\n🔮 𝗘𝗦𝗧𝗔𝗗𝗢: ${isEnable ? '𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾' : '𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾'}\n💻 𝗣𝗔𝗥𝗔: ${isAll ? '𝙴𝚂𝚃𝙴 𝙱𝙾𝚃' : isUser ? '' : '𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃'}`}, {quoted: m});
+  conn.sendMessage(m.chat, {text: `🍭 𝗢𝗣𝗖𝗜𝗢𝗡: ${type}\n☃️ 𝗘𝗦𝗧𝗔𝗗𝗢: ${isEnable ? '𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾' : '𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾'}\n🐺 𝗣𝗔𝗥𝗔: ${isAll ? '𝙴𝚂𝚃𝙴 𝙱𝙾𝚃' : isUser ? '' : '𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃'}`}, {quoted: m});
 };
 handler.help = ['en', 'dis'].map((v) => v + 'able <option>');
 handler.tags = ['group', 'owner'];
