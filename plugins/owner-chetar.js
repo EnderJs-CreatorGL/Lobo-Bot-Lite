805831180f3195aa98dd7124bcf4591d64836772
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-11 19:53:32 -0600

    Update owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `*[❗] @${m.sender.split('@')[0]} ahora tus recursos son ilimitados.*`, mentions: [m.sender]}, {quoted: m});
+        conn.sendMessage(m.chat, {text: `[🍀] @${m.sender.split('@')[0]} 𝗢𝘄𝗻𝗲𝗿 𝗮𝗵𝗼𝗿𝗮 𝘁𝗼𝗱𝗼 𝗲𝘀 𝗶𝗹𝗶𝗺𝗶𝘁𝗮𝗱𝗼, 𝘀𝗶𝗶𝗶𝗶.`, mentions: [m.sender]}, {quoted: m});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].limit = Infinity;
   global.db.data.users[m.sender].level = Infinity;
