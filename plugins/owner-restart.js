880c8a772465208c4e9cf86367042cfed4f283a5
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-11 20:11:21 -0600

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, isROwner, text}) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
   if (conn.user.jid == conn.user.jid) {
     // conn.readMessages([m.key])
-    await m.reply('🔄 𝚁𝙴𝙸𝙽𝙸𝙲𝙸𝙰𝙽𝙳𝙾 𝙰 𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃\n 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾 𝙿𝙾𝚁𝙵𝚂𝚅𝙾𝚁');
+    await m.reply('[🐺] 𝗠𝗲 𝗲𝘀𝘁𝗼𝘆 𝗿𝗲𝗶𝗻𝗶𝗰𝗶𝗮𝗻𝗱𝗼,𝗲𝘀𝗽𝗲𝗿𝗲 𝘂𝗻𝗼𝘀 𝘀𝗲𝗴𝘂𝗻𝗱𝗼𝘀 𝗽𝗼𝗿 𝗳𝗮𝘃𝗼𝗿 ... ');
     process.send('reset');
   } else throw 'eh';
 };
