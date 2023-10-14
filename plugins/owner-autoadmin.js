2dafb71de3210feb05c79b4b171a5608d99744a3
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-14 12:50:22 -0600

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -2,11 +2,11 @@
 
 const handler = async (m, {conn, isAdmin}) => {
   if (m.fromMe) return;
-  if (isAdmin) throw '[💻] 𝐎𝐰𝐧𝐞𝐫, 𝐔𝐬𝐭𝐞𝐝 𝐲𝐚 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐝𝐨𝐫 𝐝𝐞 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 𝐬𝐢𝐢𝐢.';
+  if (isAdmin) throw '[💖] 𝐎𝐖𝐍𝐄𝐑, 𝐔𝐒𝐓𝐄𝐃 𝐘𝐀 𝐄𝐒 𝐀𝐃𝐌𝐈𝐍 𝐃𝐄 𝐄𝐒𝐓𝐄 𝐆𝐑𝐔𝐏𝐎🌷.';
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   } catch {
-    await m.reply('[❗] 𝐈𝐦𝐩𝐨𝐬𝐢𝐛𝐥𝐞 𝐝𝐚𝐫𝐥𝐞 𝐩𝐨𝐝𝐞𝐫 𝐞𝐧 𝐞𝐥 𝐠𝐫𝐮𝐩𝐨');
+    await m.reply('[🥀] 𝐋𝐎 𝐒𝐈𝐄𝐍𝐓𝐎, 𝐍𝐎 𝐅𝐔𝐈 𝐂𝐀𝐏𝐀𝐙 𝐃𝐄 𝐃𝐀𝐑𝐋𝐄 𝐀𝐃𝐌𝐈𝐍🍂');
   }
 };
 handler.command = /^autoadmin$/i;
