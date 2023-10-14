b291a525ea60da92d454e6edeefe2625cbf27dff
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-14 12:56:02 -0600

    Update gc-promote.js

--- plugins/gc-promote.js
+++ plugins/gc-promote.js
@@ -21,7 +21,7 @@ const handler = async (m, {conn, usedPrefix, text}) => {
   } catch (e) {
   } finally {
     conn.groupParticipantsUpdate(m.chat, [user], 'promote');
-    conn.reply(m.chat `[🌿] 𝐀𝐇𝐎𝐑𝐀 𝐄𝐒 𝐀𝐃𝐌𝐈𝐍.`, m);
+    conn.reply(m.chat, `[🌿] 𝐀𝐇𝐎𝐑𝐀 𝐄𝐒 𝐀𝐃𝐌𝐈𝐍.`, m);
   }
 };
 handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'promote ' + v);
