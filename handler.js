fe3b10f5d9871a96d7f3bff8798b781c346335f8
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-14 23:32:23 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -7,7 +7,6 @@ import {unwatchFile, watchFile} from 'fs';
 import fs from 'fs';
 import chalk from 'chalk';
 import mddd5 from 'md5';
-import lobo from 'lobocoins';
 /**
  * @type {import('@whiskeysockets/baileys')}
  */
