0dd8487fa55be1ca94e5fb0c54b6d84912de898e
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-15 11:03:05 -0600

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,10 +1,10 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/HACHEJOTA/Hachiko-Bot-MD');
+   const res = await fetch('https://api.github.com/repos/Ender-GB-Isis777/LOBO-BOT-MD');
    const json = await res.json();
    let txt = `           *乂  B O T  -  S C R I P T  乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || 'Hachiko-Bot-MD'}\n`;
+      txt += `◦  *Nombre:* ${json?.name || 'LOBO-BOT-MD'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
