391ff494a9de9918a469aa6b28064d1568088651
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-16 20:51:59 -0600

    Update config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -365,7 +365,7 @@ const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, is
       if (!/[01]/.test(command)) return await conn.sendMessage(m.chat, {text: optionsFull}, {quoted: m});
       throw false;
   }
-  conn.sendMessage(m.chat, {text: `  𝐋𝐎𝐁𝐎-𝐁𝐎𝐓-𝐌𝐃\n  [🌹] 𝗢𝗣𝗖𝗜𝗢̃𝗡: ${type}✔️\n[🍁] 𝐄𝐒𝐓𝐀𝐃𝐎: ${isEnable ? '𝑶𝒏✔️' : '𝑶𝒇𝒇✔️'}\n[🌻] 𝐏𝐀𝐑𝐀: ${isAll ? '𝑳𝒐𝒃𝒐-𝒃𝒐𝒕-𝒎𝒅✔️' : isUser ? '' : '𝑬𝒔𝒕𝒆 𝒄𝒉𝒂𝒕✔️'}`}, {quoted: m});
+  conn.sendMessage(m.chat, {text: `    ☆𝐋𝐎𝐁𝐎-𝐁𝐎𝐓-𝐌𝐃☆\n[🌹] 𝗢𝗣𝗖𝗜𝗢̃𝗡: ${type}✔️\n[🍁] 𝐄𝐒𝐓𝐀𝐃𝐎: ${isEnable ? '𝑶𝒏✔️' : '𝑶𝒇𝒇✔️'}\n[🌻] 𝐏𝐀𝐑𝐀: ${isAll ? '𝑳𝒐𝒃𝒐-𝒃𝒐𝒕-𝒎𝒅✔️' : isUser ? '' : '𝑬𝒔𝒕𝒆 𝒄𝒉𝒂𝒕✔️'}`}, {quoted: m});
 };
 handler.help = ['en', 'dis'].map((v) => v + 'able <option>');
 handler.tags = ['group', 'owner'];
