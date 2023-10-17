c4ece9e4710508c7f6a5b7c334e8be189afc5161
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-17 11:29:37 -0600

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn, isPrems}) => {
   const hasil = Math.floor(Math.random() * 1000);
   const time = global.db.data.users[m.sender].lastmiming + 600000;
-  if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `[ ⏱️ ] 𝗘𝘀𝗽𝗲𝗿𝗮 ${msToTime(time - new Date())} 𝗣𝗮𝗿𝗮 𝗿𝗲𝘁𝗼𝗿𝗻𝗮𝗿 𝗮 𝗹𝗮 𝗺𝗶𝗻𝗲𝗿𝗶𝗮`;
-  m.reply(`[🎉]𝗠𝗶𝗻𝗲𝗿𝗶𝗮 𝗽𝗲𝗿𝗳𝗲𝗰𝘁𝗮, 𝗖𝗮𝗻𝘁𝗶𝗱𝗮𝗱 𝗺𝗶𝗻𝗮𝗱𝗮: ${hasil} 𝗫𝗣`);
+  if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `[🕕] 𝗘𝘀𝗽𝗲𝗿𝗮 ${msToTime(time - new Date())} 𝗣𝗮𝗿𝗮 𝗿𝗲𝘁𝗼𝗿𝗻𝗮𝗿 𝗮 𝗹𝗮 𝗺𝗶𝗻𝗲𝗿𝗶𝗮`;
+  m.reply(`[💝]𝗠𝗶𝗻𝗲𝗿𝗶𝗮 𝗽𝗲𝗿𝗳𝗲𝗰𝘁𝗮,𝗖𝗮𝗻𝘁𝗶𝗱𝗮𝗱 𝗺𝗶𝗻𝗮𝗱𝗮: ${hasil} 𝗫𝗣`);
   global.db.data.users[m.sender].lastmiming = new Date * 1;
 };
 handler.help = ['minar'];
