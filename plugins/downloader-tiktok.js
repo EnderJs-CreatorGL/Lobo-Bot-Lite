c0baaff0af5594ee11a963ac4eea78a6a061de28
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-18 10:59:37 -0600

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   // let buttons = [{ buttonText: { displayText: '♫ 𝙰𝚄𝙳𝙸𝙾 ♫' }, buttonId: `${usedPrefix}tomp3` }]
   try {
     const aa = {quoted: m, userJid: conn.user.jid};
-    const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: 'ʜᴀᴄʜɪᴋᴏ - ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/Ender-GB-Isis777/LOBO-BOT-MD'}, mentionedJid: [m.sender]}}}, aa);
+    const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: 'ʟᴏʙᴏ- ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/Ender-GB-Isis777/LOBO-BOT-MD'}, mentionedJid: [m.sender]}}}, aa);
     await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
     const dataF = await tiktok.v1(args[0]);
     // let desc1 =  `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${dataF.nickname || 'Indefinido'}`
