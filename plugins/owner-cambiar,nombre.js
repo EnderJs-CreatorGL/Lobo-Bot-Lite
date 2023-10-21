1f2a0c89708fb51a1ac5dd3f040d42d7534a9bc4
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-20 23:18:11 -0600

    Update owner-cambiar,nombre.js

--- plugins/owner-cambiar,nombre.js
+++ plugins/owner-cambiar,nombre.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Y EL TEXTO?`
+  if (!text) throw `¿𝙽𝙰𝙼𝙴 𝙳𝙴𝙻 𝙱𝙾𝚃 ?`
   try {
     await conn.updateProfileName(text)
-    m.reply('LISTO!')
+    m.reply('𝙻𝙸𝚂𝚃𝙾, 𝙲𝙰𝙼𝙱𝙸𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾🪁!')
   } catch (e) {
     console.log(e)
-    throw `Error`
+    throw `𝙴𝚁𝚁𝙾𝚁 𝙰𝙻 𝙲𝙰𝙼𝙱𝙸𝙰𝚁 𝙴𝙻 𝙽𝙰𝙼𝙴`
   }
 }
 handler.help = ['setbotname <teks>']
