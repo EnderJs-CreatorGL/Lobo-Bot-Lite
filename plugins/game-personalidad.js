affb72e11eb85e5fa37c44e12a013d990dc2a5d3
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-20 22:08:16 -0600

    Update game-personalidad.js

--- plugins/game-personalidad.js
+++ plugins/game-personalidad.js
@@ -2,7 +2,7 @@
 
 if (!text) return conn.reply(m.chat, ' 🪁𝗜𝗻𝗴𝗿𝗲𝘀𝗮 𝘂𝗻 𝗻𝗼𝗺𝗯𝗿𝗲(𝗻𝗮𝗺𝗲)🪁', m)
 	
-let personalidad = `*• Nombre* : ${text}
+let personalidad = `• Nombre : ${text}
 *• Buena Moral* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
 *• Mala Moral* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
 *• Tipo de persona* : ${pickRandom(['De buen corazón','Arrogante','Tacaño','Generoso','Humilde','Tímido','Cobarde','Entrometido','Cristal','No binarie XD', 'Pendejo'])}
@@ -15,9 +15,9 @@ let personalidad = `*• Nombre* : ${text}
 *• Género* : ${pickRandom(['Hombre', 'Mujer', 'Homosexual', 'Bisexual', 'Pansexual', 'Feminista', 'Heterosexual', 'Macho alfa', 'Mujerzona', 'Marimacha', 'Palosexual', 'PlayStationSexual', 'Sr. Manuela', 'Pollosexual'])}`
 conn.reply(m.chat, personalidad, fkontak, m, { mentions: conn.parseMention(personalidad) })
 }
-handler.help = ['personalidad']
+handler.help = ['identificar']
 handler.tags = ['juegos']
-handler.command = /^personalidad/i
+handler.command = /^identificar/i
 
 export default handler
 
