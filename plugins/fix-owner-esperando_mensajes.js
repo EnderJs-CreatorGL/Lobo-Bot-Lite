12b7330d0906e049f4b6bf45d2dae0c1a096d244
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-22 13:11:54 -0600

    Update fix-owner-esperando_mensajes.js

--- plugins/fix-owner-esperando_mensajes.js
+++ plugins/fix-owner-esperando_mensajes.js
@@ -30,10 +30,10 @@ const handler = async (m, { conn, usedPrefix }) => {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
     await conn.sendMessage(m.chat, {text: '*[❗] Ocurrió un error al eliminar los archivos de sesión.*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `💖𝗣𝗲𝗿𝗳𝗲𝗰𝘁𝗼,¿𝗮𝗵𝗼𝗿𝗮 𝗺𝗲 𝗹𝗲𝗲𝘀?\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
-handler.command = /^(del_reg_in_session_owner|dsowner|delhachiko|clearallsession)$/i;
+handler.command = /^(del_reg_in_session_owner|deletelobo|dellobo|clearallsession)$/i;
 handler.rowner = true
 export default handler;
