9e2ed5e6155729e5b1d7335efe517863176e83b1
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-21 21:34:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
 global.imagen5 = fs.readFileSync('./src/+18.jpg');
 global.imagen6= fs.readFileSync('./Menu3.png');
-global.imagen7= fs.readFileSync('./src/lbbot.jpg');
+global.imagen7= fs.readFileSync('./src/Misticbot.jpg');
 global.mods = [];
 
 //* *******Tiempo***************
