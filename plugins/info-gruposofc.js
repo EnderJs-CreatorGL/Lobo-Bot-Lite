b61beab06f3410be8ded136e70b0405ac1f13855
Rebelde LD <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-26 09:00:23 -0600

    Update info-gruposofc.js
    Signed-off-by: Rebelde LD <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,21 +1,21 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `𝐇𝐨𝐥𝐚 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 👋🏻, 𝐓𝐞 𝐢𝐧𝐯𝐢𝐭𝐨 𝐚 𝐮𝐧𝐢𝐫𝐭𝐞 𝐚 𝐥𝐨𝐬 𝐠𝐫𝐮𝐩𝐨𝐬 𝐎𝐅𝐂 𝐝𝐞 𝐋𝐎𝐁𝐎-𝐁𝐎𝐓-𝐌𝐃 :D
+  const text = `𝐇𝐨𝐥𝐚 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 👋, 𝐓𝐞 𝐢𝐧𝐯𝐢𝐭𝐨 𝐚 𝐮𝐧𝐢𝐫𝐭𝐞 𝐚 𝐥𝐨𝐬 𝐠𝐫𝐮𝐩𝐨𝐬 𝐎𝐅𝐂 𝐝𝐞 𝐋𝗼𝗯𝗼-𝐁𝗼𝘁-𝐌𝐃 :D
 
 ➤ 𝐆𝐫𝐮𝐩𝐨𝐬 𝐨𝐟𝐢𝐜𝐢𝐚𝐥𝐞𝐬 𝐝𝐞𝐥 𝐋𝐨𝐛𝐨:
 
-✪ 𝐆𝐑𝐔𝐏𝐎 𝐎𝐅𝐂:
+🌹 𝐆𝐑𝐔𝐏𝐎 𝐎𝐅𝐂:
  
 ༄ ${gp1} ༆
 
-✪ 𝐄𝐍𝐋𝐀𝐂𝐄𝐒 𝐋𝐎𝐁𝐎:
+🌹 𝗖𝗮𝗻𝗮𝗹 𝗟𝗼𝗯𝗼:
 ༄ ${gp2} ༄
 `.trim();
   const buttonMessage= {
     'document': {url: `https://github.com/Ender-GB-Isis777/LOBO-BOT-MD`},
     'mimetype': `application/${document}`,
-    'fileName': `「  𝐋𝐨𝐛𝐨-𝐁𝐨𝐭-𝐌𝐃 」`,
+    'fileName': `「 𝗛𝗲𝗹𝗹𝗼 𝘄𝗼𝗿𝗹 」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
