7f79427e1525c4ec9210e31c357a71434ed9b088
Rebelde LD <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-26 10:53:04 -0600

    Update downloader-ytmeta.js
    Signed-off-by: Rebelde LD <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/downloader-ytmeta.js
+++ plugins/downloader-ytmeta.js
@@ -19,11 +19,11 @@ const extract = (await ytdl.getBasicInfo(text)).videoDetails.title;
     genre: s.meta.category || "-",
     comment: {
       language: "spa",
-      text: '🤴🏻 Descarga por BrunoSobrino & TheMystic-Bot-MD 🤖',
+      text: '💖 Descarga por Ender LD y Lobo-Bot-MD 💖',
     },
     unsynchronisedLyrics: {
       language: "spa",
-      text: '🤴🏻 Descarga por BrunoSobrino & TheMystic-Bot-MD 🤖',
+      text: '💖 Descarga por Ender LD y Lobo-Bot-MD 💖',
     },
     image: {
       mime: "image/jpeg",
