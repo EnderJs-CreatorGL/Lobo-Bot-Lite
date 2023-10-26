adf6277aca373066fcb222fdf444773c30b1b2b2
Rebelde LD <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-26 11:07:57 -0600

    Update convertidor-toimg.js
    Signed-off-by: Rebelde LD <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,6 +1,6 @@
 import {webp2png} from '../lib/webp2mp4.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+  const notStickerMessage = `[❗] 𝗥𝗲𝘀𝗽𝗼𝗻𝗱𝗲 𝗮𝗹 𝘀𝘁𝗶𝗰𝗸𝗲𝗿 𝗽𝗮𝗿𝗮 𝗰𝗼𝗻𝘃𝗲𝗿𝘁𝗶𝗿 𝗮 𝗶𝗺𝗮𝗴𝗲𝗻 𝘂𝘀𝗮  ${usedPrefix + command}`;
   if (!m.quoted) throw notStickerMessage;
   const q = m.quoted || m;
   const mime = q.mediaType || '';
