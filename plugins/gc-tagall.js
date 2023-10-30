7dc999a0d5d1c881daec62beaa589f5f97ee8064
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2023-10-29 21:47:22 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,17 +5,16 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `⺀𝐋 𝐋 𝐀 𝐌 𝐀 𝐍 𝐃 𝐎 - 𝐆 𝐑 𝐔 𝐏 𝐎⺀\n\n➪ ${oi}\n\n➪ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* 𝙰𝙻𝙲𝙰𝙱𝙾𝚃💥`;
+  let teks = `*𝙼𝙴𝙽𝙲𝙸𝙾𝙽 𝙶𝙴𝙽𝙴𝚁𝙰𝙻*\n\n ✨𝙰𝙻𝙲𝙰-𝙱𝙾𝚃 \n\n💀 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `┣☢︎︎ @${mem.id.split('@')[0]}\n`;
+    teks += `💥 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└ 𝑳𝑶𝑩𝑶-𝑩𝑶𝑻-𝑴𝑫\n\n▌│█║▌║▌║║▌║▌║▌║█`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|llamar|todos|vengan)$/i;
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
