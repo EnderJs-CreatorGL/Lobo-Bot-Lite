03f0a65ad1339e8e76779d8dc2b04cc8244da0e7
Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-31 18:13:23 -0600

    Update buscador-githubsearch.js
    Signed-off-by: Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -11,12 +11,12 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   return `
 *${1 + index}. ${repo.full_name}${repo.fork ? ' (fork)' : ''}*
           ☆𝕷𝖔𝖇𝖔-𝕭𝖔𝖙-𝕸𝕯☆
-🌼√ [𝗨𝗥𝗟: ${repo.html_url}
-🌹√ [𝗖𝗿𝗲𝗮𝗱𝗼: ${formatDate(repo.created_at)}
-🍀√ [𝗔𝗰𝘁𝘂𝗮𝗹𝗶𝘇𝗮𝗰𝗶𝗼𝗻 : ${formatDate(repo.updated_at)}
-🌐√ [𝗧𝗼𝘁𝗮𝗹 𝗰𝗹𝗼𝗻𝗲: $ git clone ${repo.clone_url}
-🌟√ [𝗧𝗼𝘁𝗮𝗹 𝘃𝗶𝘀𝘁𝗮𝘀: ${repo.watchers} ◉ 🌸 √[𝗙𝗼𝗿𝗸𝘀: ${repo.forks} ◉ ✨ √[𝗘𝘀𝘁𝗿𝗲𝗹𝗹𝗮𝘀: ${repo.stargazers_count} ◉ ☃️ √[𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻:
-${repo.description ? `📄 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻:\n${repo.description}` : ''}
+🌼√ [𝗨𝗥𝗟]: ${repo.html_url}.
+🌹√ [𝗖𝗿𝗲𝗮𝗱𝗼]: ${formatDate(repo.created_at)}.
+🍀√ [𝗔𝗰𝘁𝘂𝗮𝗹𝗶𝘇𝗮𝗰𝗶𝗼𝗻]: ${formatDate(repo.updated_at)}.
+🌐√ [𝗧𝗼𝘁𝗮𝗹 𝗰𝗹𝗼𝗻𝗲]: $ git clone ${repo.clone_url}.
+🌟√ [𝗧𝗼𝘁𝗮𝗹 𝘃𝗶𝘀𝘁𝗮𝘀]: ${repo.watchers}. ◉ 🌸 √[𝗙𝗼𝗿𝗸𝘀: ${repo.forks}. ◉ ✨ √[𝗘𝘀𝘁𝗿𝗲𝗹𝗹𝗮𝘀]: ${repo.stargazers_count}. ◉ ☃️√ [𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻]:
+${repo.description ? .`[📄√ 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻]:\n${repo.description}.` : ''}
 `.trim()}).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendMessage(m.chat, {text: str.trim()}, {quoted: m})
 //conn.sendMessage(m.chat, {text: str.trim(), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen, "mediaUrl": global.md}}}, {quoted: m});  
