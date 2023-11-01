c5d6dac687c5962a9bc58b74e693eeceef81d0b6
Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-31 18:40:15 -0600

    Update owner-noetiquetar.js
    Signed-off-by: Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,11 +1,11 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '𝐍𝐎-𝐄𝐓𝐈𝐐𝐔𝐄𝐓𝐀𝐑', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/50558124470', thumbnail: imagen7}}})
+conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '𝕷𝖔𝖇𝖔-𝕭𝖔𝖙-𝕸𝕯', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/50558124470'}}})
 }
 handler.customPrefix = /@50558124470/i;
 handler.command = new RegExp();
 
 export default handler;
 
-//conn.reply(m.chat, `hhh`, m, { contextInfo:{ externalAdReply: {title: 'titulo', body: 'cuero', sourceUrl: global.md, thumbnail: imagen7}}})
+//conn.reply(m.chat, `hhh`, m, { contextInfo:{ externalAdReply: {title: 'titulo', body: 'cuero', sourceUrl: global.md}}})
