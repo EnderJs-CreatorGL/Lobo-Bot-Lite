45443d4ac7355ed0b91dab3695d227c0d75a1fad
Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-10-31 18:03:23 -0600

    Update buscador-githubsearch.js
    Signed-off-by: Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `[❗] 𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐞𝐥 𝐭𝐞𝐱𝐭𝐨 𝐩𝐚𝐫𝐚 𝐢𝐧𝐢𝐜𝐢𝐚𝐫 𝐛𝐮𝐬𝐪𝐮𝐞𝐝𝐚, 𝐄𝐣𝐞𝐦𝐩𝐥𝐨: ${usedPrefix + command} 𝐋𝐎𝐁𝐎-𝐁𝐎𝐓-𝐌𝐃`;
+  if (!text) throw `[✒️] 𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐞𝐥 𝐭𝐞𝐱𝐭𝐨 𝐩𝐚𝐫𝐚 𝐢𝐧𝐢𝐜𝐢𝐚𝐫 𝐛𝐮𝐬𝐪𝐮𝐞𝐝𝐚, 𝐄𝐣𝐞𝐦𝐩𝐥𝐨: ${usedPrefix + command} 𝕷𝖔𝖇𝖔-𝕭𝖔𝖙-𝕸𝕯`;
   const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
     q: text,
   }));
@@ -10,12 +10,12 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   const str = json.items.map((repo, index) => {
   return `
 *${1 + index}. ${repo.full_name}${repo.fork ? ' (fork)' : ''}*
-🔗 𝐔𝐑𝐋: ${repo.html_url}
-📅 𝐂𝐫𝐞𝐚𝐝𝐨 𝐞𝐥: ${formatDate(repo.created_at)}
-🔄 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐨 𝐞𝐥: ${formatDate(repo.updated_at)}
-📥 𝐂𝐥𝐨𝐧𝐞: $ git clone ${repo.clone_url}
-👁 𝐕𝐢𝐬𝐭𝐚𝐬: ${repo.watchers} ◉ 🍴 *𝙵𝙾𝚁𝙺𝚂:* ${repo.forks} ◉ ⭐ *𝙴𝚂𝚃𝚁𝙴𝙻𝙻𝙰𝚂:* ${repo.stargazers_count} ◉ ❓ *𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:*
-${repo.description ? `📝 *Descripción:*\n${repo.description}` : ''}
+🌼 𝗨𝗥𝗟: ${repo.html_url}
+🌹 𝗖𝗿𝗲𝗮𝗱𝗼: ${formatDate(repo.created_at)}
+🍀 𝗔𝗰𝘁𝘂𝗮𝗹𝗶𝘇𝗮𝗰𝗶𝗼𝗻 : ${formatDate(repo.updated_at)}
+🌐 𝗧𝗼𝘁𝗮𝗹 𝗰𝗹𝗼𝗻𝗲: $ git clone ${repo.clone_url}
+🌟 𝗧𝗼𝘁𝗮𝗹 𝘃𝗶𝘀𝘁𝗮𝘀: ${repo.watchers} ◉ 🌸 𝗙𝗼𝗿𝗸𝘀: ${repo.forks} ◉ ✨ 𝗘𝘀𝘁𝗿𝗲𝗹𝗹𝗮𝘀: ${repo.stargazers_count} ◉ ☃️ 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻:
+${repo.description ? `📄 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻:\n${repo.description}\n\n𝗢𝗙𝗖 𝗟𝗼𝗯𝗼:https://github.com/Ender-GB-Isis777/LOBO-BOT-MD` : ''}
 `.trim()}).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendMessage(m.chat, {text: str.trim()}, {quoted: m})
 //conn.sendMessage(m.chat, {text: str.trim(), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen, "mediaUrl": global.md}}}, {quoted: m});  
