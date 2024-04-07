8045b9454dce3aaf24915172a4ba62751f3c58ae
Ender Tallyclerk <142455051+Ender-GB-Isis777@users.noreply.github.com>
2024-04-07 14:19:56 -0600

    Create owner-saveplugin.js
    Signed-off-by: Ender Tallyclerk <142455051+Ender-GB-Isis777@users.noreply.github.com>

---
+++ plugins/owner-saveplugin.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+let handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) return conn.reply(m.chat, `*Y EL TEXTO?*`)
+    if (!m.quoted.text) return conn.reply(m.chat, `𝗠𝗲𝗻𝗰𝗶𝗼𝗻𝗲 𝗲𝗹 𝗰𝗼𝗱𝗶𝗴𝗼 𝗮 𝗴𝘂𝗮𝗿𝗱𝗮𝗿☆`)
+    let path = `plugins/${text}.js`
+    await fs.writeFileSync(path, m.quoted.text)
+    await conn.reply(m.chat, `Guardado en ${path}`)
+}
+handler.help = ['saveplugin'].map(v => v + ' <nombre>')
+handler.tags = ['owner']
+handler.command = ["salvar", "saveplugin"]
+
+handler.rowner = true
+export default handler
