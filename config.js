d07d9ab2cdd4e11f3e4e650bbf05cb5c0df244ae
Ender Tallyclerk <142455051+Ender-GB-Isis777@users.noreply.github.com>
2024-04-08 19:10:28 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-['50558124470', '👑 𝙲𝚁𝙴𝙰𝙳𝙾𝚁 👑', true],
+['50558124470', '👑 Ender Tally 👑', true],
 ['573012482597', '𝙳𝙸𝙴𝙶𝙾 🚩', true]]
   
 global.suittag = ['50558124470']
