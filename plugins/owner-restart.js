436321c46924d1da16b1ffa610da2c5f1db22823
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-10 17:34:51 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,14 +1,20 @@
-import {spawn} from 'child_process';
-const handler = async (m, {conn, isROwner, text}) => {
-  if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
-  if (conn.user.jid == conn.user.jid) {
-    // conn.readMessages([m.key])
-    await m.reply('[🌹] 𝗘𝗷𝗲𝗰𝘂𝘁𝗮𝗻𝗱𝗼 𝗢𝗿𝗱𝗲𝗻 𝗲𝘀𝗽𝗲𝗿𝗲 𝘂𝗻𝗼𝘀 𝘀𝗲𝗴𝘂𝗻𝗱𝗼𝘀 𝗟𝗼𝗯𝗼-𝗕𝗼𝘁-𝗟𝗶𝘁𝗲🌼 ... ');
-    process.send('reset');
-  } else throw 'eh';
-};
-handler.help = ['restart'];
-handler.tags = ['owner'];
-handler.command = ['data000', 'datos0'];
-handler.rowner = true;
-export default handler;
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
+//await conn.sendMessage(m.chat, { que: { texto: 'Isaac Roba Codigo', key: m.key } })
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    if (conn.user.jid == conn.user.jid) {
+    const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key});
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key});
+await conn.sendMessage(m.chat, {text: `╰⊱🌩⊱ *REINICIANDO* ⊱🌩⊱╮`, edit: key});
+    process.send('reset')
+  } else throw 'eh'
+}
+handler.help = ['restart']
+handler.tags = ['owner']
+handler.command = ['data000','datos0', 'restart', 'reiniciar'] 
+handler.rowner = true
+export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
