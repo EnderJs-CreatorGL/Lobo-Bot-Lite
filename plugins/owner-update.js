5c8078a6dae09e8c83ec83bbe87cb70186092b25
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-09 20:16:25 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,23 +1,44 @@
-import {execSync} from 'child_process';
+import { execSync } from 'child_process'
 
-const handler = async (m, {conn, text}) => {
-  try {
-    if (global.conn.user.jid == conn.user.jid) {
-      const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
-      conn.reply(m.chat, stdout.toString(), m);
-    }
-  } catch (error) {
-    console.error(error);
-    let errorMessage = 'An error occurred while executing the command.';
-    if (error.message) {
-      errorMessage += '\nError message: ' + error.message;
-    }
-    await conn.reply(m.chat, errorMessage, m);
-  }
-};
+var handler = async (m, { conn, text }) => {
+//await conn.sendMessage(m.chat, { que: { texto: 'Isaac Roba Codigo', key: m.key } })
+try {
+const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+let messager = stdout.toString()
+if (messager.includes('Already up to date.')) messager = '🚩 *NO HAY NADA QUE ACTUALIZAR.*'
+if (messager.includes('Updating')) messager = '🚩 *LOBO ACTUALIZADO CORRECTAMENTE.*\n\n' + stdout.toString()
+conn.reply(m.chat, messager,)
+} catch { 
+try {
+const status = execSync('git status --porcelain')
+if (status.length > 0) {
+const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('sessions/') || line.includes('npm-debug.log')) {
+return null
+}
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
+if (conflictedFiles.length > 0) {
+const errorMessage = `🚩 *El bot se ha actualizado desde el servidor y estra conflicto  con el comando update.*\n\nArchivos En Conflicto:\n\n${conflictedFiles.join('\n')}`
+await conn.reply(m.chat, errorMessage,)
+}
+}
+} catch (error) {
+console.error(error)
+let errorMessage2 = '🔵 *Api Caida*'
+if (error.message) {
+errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+}
+await conn.reply(m.chat, errorMessage2,)
+}
+}
 
-handler.help = ['update'];
-handler.tags = ['owner'];
-handler.command = /^update|repo1$/i;
-handler.rowner = true;
-export default handler;
+}
+handler.help = ['update', 'actualizar']
+handler.tags = ['own']
+handler.command = /^(update|actualizar|gitpull)$/i
+handler.rowner = true
+//handler.limit = false
+//handler.private = false
+
+
+export default handler
