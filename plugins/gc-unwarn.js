9a0af99d95d1615022942e5b7e70318e4aedeace
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-04-27 06:37:59 -0600

    Actualizar gc-unwarn.js

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -5,12 +5,12 @@ const handler = async (m, {conn, text, command, usedPrefix}) => {
   else who = m.chat;
   const user = global.db.data.users[who];
   const bot = global.db.data.settings[conn.user.jid] || {};
-  const warntext = `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} @${global.suittag}*`;
+  const warntext = `[🌹] ᴍᴇɴᴄɪᴏɴᴀ ᴀ ʟᴀ ᴘᴇʀsᴏɴᴀ\n\n—◉ ᴇᴊᴇᴍᴘʟᴏ:\n*${usedPrefix + command} @${global.suittag}*`;
   if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
   if (m.mentionedJid.includes(conn.user.jid)) return;
-  if (user.warn == 0) throw '*[❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝚃𝙸𝙴𝙽𝙴 0 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂*';
+  if (user.warn == 0) throw '[📍] ᴇʟ ᴜsᴜᴀʀɪᴏ ᴛɪᴇɴᴇ 0 ᴀᴅᴠᴇʀᴛᴇɴᴄɪᴀs';
   user.warn -= 1;
-  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`} 𝚂𝙴 𝙻𝙴 𝚀𝚄𝙸𝚃𝙾 𝚄𝙽𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰\n*ADVERTENCIAS ${user.warn}/3*`, null, {mentions: [who]});
+  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `✔️ *@${who.split`@`[0]}*`} 𝑆𝑒 𝑙𝑒 𝑒𝑙𝑖𝑚𝑖𝑛𝑜 𝑢𝑛𝑎 𝑎𝑑𝑣𝑒𝑟𝑡𝑒𝑛𝑐𝑖𝑎 /n𝐴𝑑𝑣𝑒𝑟𝑡𝑒𝑛𝑐𝑖𝑎𝑠${user.warn}/3*`, null, {mentions: [who]});
 };
 handler.command = /^(unwarn|delwarn|deladvertir|deladvertencia|delwarning)$/i;
 handler.group = true;
