c546b3bdbbb27fce1b4bcd20627115132aa1d58f
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-04-27 06:53:42 -0600

    Actualizar owner-bcgc2.js

--- plugins/owner-bcgc2.js
+++ plugins/owner-bcgc2.js
@@ -6,7 +6,7 @@ const handler = async (m, {conn, text, participants, isAdmin, isOwner, usedPrefi
   for (const id of groups) {
     await conn.sendMessage(id, {forward: m.quoted.fakeObj, mentions: (await conn.groupMetadata(`${id}`)).participants.map((v) => v.id)}, {quoted: fproducto});
   }
-  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 ${groups.length} 𝙶𝚁𝚄𝙿𝙾/𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*`);
+  m.reply(`[🌹] ᴇᴊᴇᴄᴜᴛᴇ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ ᴀ ʟᴀ ᴘᴇʀғᴇᴄᴄɪᴏɴ ${groups.length} ɢʀᴜᴘᴏs ᴛᴏᴛᴀʟᴇs`);
 };
 handler.help = ['bcgc2'];
 handler.tags = ['owner'];
