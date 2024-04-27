991157e87f314b131280881338dcc0f443575145
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-04-27 06:35:14 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `©𝐋 𝐋 𝐀 𝐌 𝐀 𝐍 𝐃 𝐎 ™-™ 𝐆 𝐑 𝐔 𝐏 𝐎®\n\n➪ ${oi}\n\n➪ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `©sᴏʟɪᴄɪᴛᴀɴᴅᴏ ʟᴀ ᴘʀᴇsᴇɴᴄɪᴀ ɢʀᴜᴘᴀʟ ®\n\n➪ ${oi}\n\n➪ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
     teks += `┣§ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└ ™𝑳𝑶𝑩𝑶-𝑩𝑶𝑻™\n\n▌│█║▌║▌║║▌║▌║▌║█`;
+  teks += `└ ™𝘓𝘰𝘣𝘰-𝘉𝘰𝘵-𝘓𝘪𝘵𝘦™\n\n▌│█║▌║▌║║▌║▌║▌║█`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
