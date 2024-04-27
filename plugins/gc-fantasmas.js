6c1feef5e4ed077ffb0cf7520a1f8973b6a897e0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-27 10:32:57 -0500

    Actualizar gc-fantasmas.js

--- plugins/gc-fantasmas.js
+++ plugins/gc-fantasmas.js
@@ -1,30 +1,65 @@
-const handler = async (m, {conn, text, participants}) => {
-  const member = participants.map((u) => u.id);
-  if (!text) {
-    var sum = member.length;
-  } else {
-    var sum = text;
-  }
-  let total = 0;
-  const sider = [];
-  for (let i = 0; i < sum; i++) {
-    const users = m.isGroup ? participants.find((u) => u.id == member[i]) : {};
-    if ((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) {
-      if (typeof global.db.data.users[member[i]] !== 'undefined') {
-        if (global.db.data.users[member[i]].whitelist == false) {
-          total++;
-          sider.push(member[i]);
-        }
-      } else {
-        total++;
-        sider.push(member[i]);
-      }
-    }
-  }
-  if (total == 0) return conn.reply(m.chat, `[🪁] 𝑁𝑜 𝑠𝑒 𝑒𝑛𝑐𝑢𝑒𝑛𝑡𝑟𝑎𝑛 𝐹𝑎𝑛𝑡𝑎𝑠𝑚𝑎𝑠 𝑒𝑛 𝑒𝑙 𝑔𝑟𝑢𝑝𝑜. `, m);
-  m.reply(`*[ 🌹 𝐴𝑛𝑎𝑙𝑖𝑠𝑎𝑛𝑑𝑜 𝐹𝑢𝑙𝑙 🌹 ]*\n\n*𝙶𝚁𝚄𝙿𝙾:* ${await conn.getName(m.chat)}\n*𝙼𝙸𝙴𝙼𝙱𝚁𝙾𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾:* ${sum}\n\n[🎭𝐹𝑎𝑛𝑡𝑎𝑠𝑚𝑎𝑠 🎭]\n${sider.map((v) => '  • @' + v.replace(/@.+/, '')).join('\n')}\n\n𝐴𝑛𝑎𝑙𝑖𝑠𝑖𝑠 𝑐𝑜𝑚𝑝𝑙𝑒𝑡𝑎𝑑𝑜 𝑎𝑙 100%.`, null, {mentions: sider});
-};
-handler.command = /^(verfantasmas|fantasmas|sider)$/i;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+import { areJidsSameUser } from '@whiskeysockets/baileys'
+
+var handler = async (m, { conn, text, participants, args, command }) => {
+
+let member = participants.map(u => u.id)
+if(!text) {
+var sum = member.length
+} else {
+var sum = text} 
+var total = 0
+var sider = []
+for (let i = 0; i < sum; i++) {
+let users = m.isGroup ? participants.find(u => u.id == member[i]) : {}
+if ((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
+if (typeof global.db.data.users[member[i]] !== 'undefined'){
+if (global.db.data.users[member[i]].whitelist == false){
+total++
+sider.push(member[i])}
+}else {
+total++
+sider.push(member[i])}}}
+const delay = time => new Promise(res=>setTimeout(res,time))
+
+switch (command) {
+
+
+
+case 'fantasmas': 
+if(total == 0) return conn.reply(m.chat, `[🪁] 𝑁𝑜 𝑠𝑒 𝑒𝑛𝑐𝑢𝑒𝑛𝑡𝑟𝑎𝑛 𝐹𝑎𝑛𝑡𝑎𝑠𝑚𝑎𝑠 𝑒𝑛 𝑒𝑙 𝑔𝑟𝑢𝑝𝑜.`, m, ) 
+m.reply(`[ 🌹 𝐴𝑛𝑎𝑙𝑖𝑠𝑎𝑛𝑑𝑜 𝐹𝑢𝑙𝑙 🌹 ]\n\n[🎭𝐹𝑎𝑛𝑡𝑎𝑠𝑚𝑎𝑠 🎭]\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*📝 NOTA:*\nEsto no es al 100% acertado, el bot inicia el conteo de mensajes a partir de que se active en este número`, null, { mentions: sider }) 
+break
+
+case 'kickfantasmas':  
+if(total == 0) return conn.reply(m.chat, `🎌 *Este grupo es activo no tiene fantasmas*`, m, ) 
+await m.reply(`🚩 *Eliminación de inactivos*\n\n⚠️ *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n❗ _El bot eliminara a los usuarios de la lista mencionada cada 10 segundos_`, null, { mentions: sider }) 
+await delay(1 * 10000)
+let chat = global.db.data.chats[m.chat]
+chat.welcome = false
+try {
+
+let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
+let kickedGhost = sider.map(v => v.id).filter(v => v !== conn.user.jid)
+for (let user of users)
+if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin)
+{
+let res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+kickedGhost.concat(res)
+await delay(1 * 10000)
+}} finally{
+chat.welcome = true
+}
+break            
+}
+
+}
+handler.tags = ['grupo']
+handler.command = /^(fantasmas|kickfantasmas)$/i
+handler.group = true
+handler.botAdmin = true
+handler.admin = true
+handler.fail = null
+
+export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
