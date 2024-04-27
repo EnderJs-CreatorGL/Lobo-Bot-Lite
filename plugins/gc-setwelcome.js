d574a8d2a944ea65608df54350b2143d7d8277e0
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-04-27 06:01:46 -0600

    Actualizar gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn, text, isROwner, isOwner}) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text;
-    m.reply('[🧸] 𝗠𝗲𝗻𝘀𝗮𝗷𝗲 𝗰𝗼𝗻𝗳𝗶𝗴𝘂𝗿𝗮𝗱𝗼 𝗲𝘅𝗶𝘁𝗼');
-  } else throw `[❗] 𝗜𝗻𝗴𝗿𝗲𝘀𝗮 𝗲𝗹 𝗺𝗲𝗻𝘀𝗮𝗷𝗲 𝗱𝗲 𝗯𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗮 𝗽𝗿𝗶𝗺𝗲𝗿𝗼, 𝘂𝘀𝗲:\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
+    m.reply('[🎭] 𝗠𝗲𝗻𝘀𝗮𝗷𝗲 𝗰𝗼𝗻𝗳𝗶𝗴𝘂𝗿𝗮𝗱𝗼 𝗲𝘅𝗶𝘁𝗼');
+  } else throw `[🪁] 𝗜𝗻𝗴𝗿𝗲𝘀𝗮 𝗲𝗹 𝗺𝗲𝗻𝘀𝗮𝗷𝗲 𝗱𝗲 𝗯𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗮 𝗽𝗿𝗶𝗺𝗲𝗿𝗼, 𝘂𝘀𝗲:\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
 };
 handler.help = ['setwelcome <text>'];
 handler.tags = ['group'];
