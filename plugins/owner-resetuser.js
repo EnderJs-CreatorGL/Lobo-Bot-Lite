6846061f1bc4e64f3fc9b6590b80b83509889709
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-04-27 09:42:10 -0600

    Actualizar owner-resetuser.js

--- plugins/owner-resetuser.js
+++ plugins/owner-resetuser.js
@@ -11,20 +11,20 @@ const handler = async (m, { conn, text }) => {
             const number = quotedNumberMatches.join('');
             user = number + '@s.whatsapp.net';
         } else {
-        return conn.sendMessage(m.chat, {text: `*[❗] Formato de usuario no reconocido. Responda a un mensaje, etiquete a un usuario o escriba su número de usuario.*`}, {quoted: m});
+        return conn.sendMessage(m.chat, {text: `[🌹] ᴇʀʀᴏʀ ɴᴏ ʀᴇᴄᴏɴᴏᴄɪᴅᴏ`}, {quoted: m});
     }
     } else {
-        return conn.sendMessage(m.chat, {text: `*[❗] Formato de usuario no reconocido. Responda a un mensaje, etiquete a un usuario o escriba su número de usuario.*`}, {quoted: m});
+        return conn.sendMessage(m.chat, {text: `[🌹] ᴇʀʀᴏʀ ɴᴏ ʀᴇᴄᴏɴᴏᴄɪᴅᴏ`}, {quoted: m});
     }        
         const groupMetadata = m.isGroup ? await conn.groupMetadata(m.chat) : {};
         const participants = m.isGroup ? groupMetadata.participants : [];
         const users = m.isGroup ? participants.find(u => u.jid == user) : {};
         const userNumber = user.split('@')[0];
         if (!global.global.db.data.users[user] || global.global.db.data.users[user] == '') {
-            return conn.sendMessage(m.chat, {text: `*[❗] El usuario @${userNumber} no se encuentra en mi base de datos.*`, mentions: [user]}, {quoted: m});
+            return conn.sendMessage(m.chat, {text: `[🧸] ᴇʟ ᴜsᴇʀ @${userNumber} ɴᴏ ᴇsᴛᴀ ᴇɴ ʟᴀ ᴅᴀᴛᴀ ʙᴀsᴇ`, mentions: [user]}, {quoted: m});
          }
         delete global.global.db.data.users[user];
-        conn.sendMessage(m.chat, {text: `[❗] 𝗣𝗲𝗿𝗳𝗲𝗰𝘁𝗼, 𝗵𝗲 𝗯𝗼𝗿𝗿𝗮𝗱𝗼 𝗹𝗼𝘀 𝗱𝗮𝘁𝗼𝘀 𝗱𝗲𝗹 𝘂𝘀𝘂𝗮𝗿𝗶𝗼 @${userNumber} 𝗱𝗲 𝗹𝗮 𝗱𝗮𝘁𝗮𝗯𝗮𝘀𝗲 𝗱𝗲 𝗟𝗼𝗯𝗼-𝗯𝗼𝘁-𝗠𝗗.`, mentions: [user]}, {quoted: m});
+        conn.sendMessage(m.chat, {text: `[🌹] ᴘᴇʀғᴇᴄᴛᴏ @${userNumber} ᴅᴇ ʟᴀ ᴅᴀᴛᴀ ʙᴀsᴇ ᴅᴇ ʟᴏʙᴏ-ʙᴏᴛ-ʟɪᴛᴇ`, mentions: [user]}, {quoted: m});
 };
 handler.tags = ['owner'];
 handler.command = /(data0|deletedatauser|datos0)$/i;
