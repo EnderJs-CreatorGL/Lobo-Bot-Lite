cb62f856d2dd33e0c7b2a8acf6da08e383390588
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-01 12:58:45 -0600

    Actualizar menu-caja_fuerte.js

--- plugins/menu-caja_fuerte.js
+++ plugins/menu-caja_fuerte.js
@@ -1,11 +1,17 @@
-const handler = async (m, {conn, usedPrefix}) => {
-  const pp = imagen4;
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   try {
-  } catch (e) {
-  } finally {
-    const name = await conn.getName(m.sender);
-    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const str = `
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+
+  const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+  const {money, joincount} = global.db.data.users[m.sender];
+  const {exp, limit, level, role} = global.db.data.users[m.sender];
+  const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+    let fkon = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    // await conn.reply(m.chat, '𝐸𝑗𝑒𝑐𝑢𝑡𝑎𝑛𝑑𝑜 𝑎𝑐𝑐𝑖𝑜̀𝑛,𝐸𝑛𝑣𝑖𝑎𝑛𝑑𝑜📍. . .', m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '•𝐿𝑜𝑏𝑜-𝐵𝑜𝑡-𝐿𝑖𝑡𝑒🐈🌻!!', body: 'bienvenido', sourceUrl: global.md, thumbnail: imagen1 }}})
+//m.react('🐺');
+    await conn.sendMessage(m.chat, { react: { text: '💖', key: m.key } })
+  let txt =`
 ┏━━━━━━━━━━━━━━━━┓
 ┃ ✦⏤͟͟͞͞𝗖𝗔𝗝𝗔 𝗙𝗨𝗘𝗥𝗧𝗘⏤͟͟͞͞✦
 ┗━━━━━━━━━━━━━
