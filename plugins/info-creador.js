3e33385cbef19e9bd7e296d41fc78c014d949166
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-03 18:02:14 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -12,7 +12,7 @@ let name = await conn.getName(who)
 
   await sendContactArray(conn, m.chat, [
      [`${nomorown}`, `${await conn.getName('50558124470'+'@s.whatsapp.net')}`, `💖 Creador`, saludo, 'argumedod44@gmail.com', `🇳🇮 Nicaragua`, `https://www.youtube.com/@EnderOwner`, bio],
-[`${conn.user.jid.split('@')[0]}`, `𝗘𝘀 𝗨𝗻 𝗕𝗼𝘁 🍁`, `${await conn.getName(conn.user.jid)}`, `📵 No Hacer Spam`, 'thelobobot@gmail.com', `🇳🇮 Nicaragua`, `https://github.com/EnderJs-CreatorGL/Lobo-Bot-Lite`, biobot],  [`${numero}`, `𝗢𝘄𝗻𝗲𝗿 𝗢𝗳𝗰 🗒️`, `${await conn.getName(conn.user.jid)}`, `🏷 Solo Cosas Importantes`, 'theyotsubanakano@gmail.com', `🇨🇴 Colombia`, `https://github.com/Diego-YL-177/`, `_✨️ L'amore è una cosa meravigliosa 💖_`]
+[`${conn.user.jid.split('@')[0]}`, `𝗘𝘀 𝗨𝗻 𝗕𝗼𝘁 🍁`, `${await conn.getName(conn.user.jid)}`, `📵 No Hacer Spam`, 'thelobobot@gmail.com', `🇳🇮 Nicaragua`, `https://github.com/EnderJs-CreatorGL/Lobo-Bot-Lite`, biobot],  [`${numero}`, `𝗢𝘄𝗻𝗲𝗿 𝗢𝗳𝗰 🗒️`, `${await conn.getName('573012482597'+'@s.whatsapp.net')}`, `🏷 Solo Cosas Importantes`, 'theyotsubanakano@gmail.com', `🇨🇴 Colombia`, `https://github.com/Diego-YL-177/`, `✨️ L'amore è una cosa meravigliosa 💖`]
 ], m)
   //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creador, no hagas spam!!`)
   } 
