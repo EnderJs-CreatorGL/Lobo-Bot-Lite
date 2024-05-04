8e81220f9e20307a30e98b229d25e44df1017443
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 11:39:57 -0500

    Actualizar di.js

--- plugins/di.js
+++ plugins/di.js
@@ -1,6 +1,6 @@
 //Comando por: diegojadibot
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
+/*let handler = async(m, { conn, text, usedPrefix, command }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
             const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -22,4 +22,4 @@ handler.help = ['di']
 handler.tags = ['decir']
 handler.command = /^(decir|di|buscar-novio|buscar-novia|buscarnovio|buscarnovia)$/i
 handler.register = true
-export default handler
+export default handler*/
