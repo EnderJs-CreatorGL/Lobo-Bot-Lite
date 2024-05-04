5571cf34678e9f79aa4970b7340694fd32df0593
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 10:33:22 -0500

    Actualizar owner-noetiquetar.js

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,7 +1,7 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: packname, body: saludo, sourceUrl: md, thumbnail: imagen1 }}})
+conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*`, m, fake, { contextInfo:{ externalAdReply: {title: packname, body: saludo, sourceUrl: md, thumbnail: imagen1 }}})
 }
 handler.customPrefix = /@50558124470/i;
 handler.command = new RegExp();
