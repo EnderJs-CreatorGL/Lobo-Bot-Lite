7229f3f2f13332584b440d8ec9f99229f3b29742
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-05 14:15:56 -0500

    Actualizar bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -1,4 +1,4 @@
-let media = './Menu.png' 
+let media = './Menu2.jpg' 
  let handler = async (m, { conn, command }) => { 
  let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" } 
 
@@ -6,10 +6,10 @@ let media = './Menu.png'
 
  let menu = `👋 _¿Hola, ${nombre} como estas?_ 
 
-!allmenu
-_(Para ver el menú completo)_`
+use .menu2 / .allmenu
+  _(Para ver el menú completo)_`
 
-await conn.sendFile(m.chat, media, 'lobo.jpg', menu, fkontak, m, fake)} 
+await conn.sendFile(m.chat, media, 'Curiosity.jpg', menu, fkontak, m, fake)} 
  handler.tags = ['main'] 
  handler.command = /^menu$/i 
  handler.exp = 35 
