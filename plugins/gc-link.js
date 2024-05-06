f6dd6d7b7ac977bc2f8b918608d93374d50480e3
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-05 21:34:32 -0600

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,8 +3,8 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: '𝑳𝑰𝑵𝑲 𝑮𝑹𝑶𝑼𝑷',
-      body: '𝐿𝑜𝑏𝑜-𝐵𝑜𝑡-𝐿𝑖𝑡𝑒',
+      title: '𝐺𝑟𝑢𝑝𝑜 𝐿𝑖𝑛𝑘√',
+      body: '𝐿𝑜𝑏𝑜-𝐵𝑜𝑡-𝐿𝑖𝑡𝑒🐈🌻',
       previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
       sourceUrl: global.md}}});
 };
