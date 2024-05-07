a1c1f7fb6a54d907c4c32b78df31bb55208788b6
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-07 13:33:36 -0600

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,9 +1,9 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = true;
-  m.reply('[🌼]𝗖𝗵𝗮𝘁 𝗯𝗮𝗻𝗲𝗮𝗱𝗼 𝗰𝗼𝗻 𝗲𝘅𝗶𝘁𝗼,𝗻𝗼 𝗽𝗼𝗱𝗿𝗮𝗻 𝘂𝘀𝗮𝗿 ʟᴏʙᴏ-ʙᴏᴛ-ʟɪᴛᴇ🥀');
+  m.reply('[🌼]𝐶ℎ𝑎𝑡 𝑝𝑟𝑜ℎ𝑖𝑏𝑖𝑑𝑜 𝑐𝑜𝑛 𝑒𝑥𝑖𝑡𝑜,𝑌𝑎 𝑛𝑜 𝑝𝑢𝑒𝑑𝑒𝑛 𝑢𝑠𝑎𝑟 𝐿𝑜𝑏𝑜-𝐵𝑜𝑡-𝐿𝑖𝑡𝑒🐈🌻');
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];
-handler.command = /^prohibirchat$/i;
+handler.command = /^prohibirchat|pschat$/i;
 handler.rowner = true;
 export default handler;
