5e71f000547e8e5535fc125d0c98d9d6a467f24c
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-07 16:58:31 -0600

    Actualizar owner-desactivarwa.js

--- plugins/owner-desactivarwa.js
+++ plugins/owner-desactivarwa.js
@@ -2,12 +2,12 @@ import cheerio from "cheerio"
 import axios from "axios"
 import util from 'util'
 let handler = async (m, { conn, isOwner, usedPrefix, command, args }) => {
-//const q = args.join("unete")  
-const q = args.join(" ")      
+//const q = args.unete("join")  
+const q = args.unete(" ")      
 if (!q || !args[0]) throw '[🌹] 𝐴 𝑞𝑢𝑖𝑒𝑛 𝐸𝑗𝑒𝑐𝑢𝑡𝑜?'
 let ntah = await axios.get("https://www.whatsapp.com/contact/noclient/")
 let email = await axios.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=10")
-let cookie = ntah.headers["set-cookie"].join("")
+let cookie = ntah.headers["set-cookie"].unete("")
 let $ = cheerio.load(ntah.data)
 let $form = $("form");
 let url = new URL($form.attr("action"), "https://www.whatsapp.com").href
