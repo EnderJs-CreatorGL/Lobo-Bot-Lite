0e5ce900e4855ee460d3ee87c27bb02773465944
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-09 18:19:29 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1456,7 +1456,7 @@ export async function participantsUpdate({id, participants, action}) {
               return;
             }
 
-    this.sendMessage(id, { text: tex, contextInfo:{ mentionedJid:[user], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 A D I O S 乂', "body": '${wm}', "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": apii.data, "sourceUrl": md}}})
+    this.sendMessage(id, { tex: tex, contextInfo:{ mentionedJid:[user], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 A D I O S 乂', "body": '${wm}', "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": apii.data, "sourceUrl": md}}})
 
             this.sendMessage(id, { text: text, contextInfo:{ mentionedJid:[user], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 ＷＥＬＣＯＭＥ 乂', "body": '${wm}', "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": apii.data, "sourceUrl": md}}})
           }
