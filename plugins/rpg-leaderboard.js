fbbb30ff77e9c959c537be2cea33ae995e948a3f
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-09 10:22:35 -0600

    Actualizar rpg-leaderboard.js

--- plugins/rpg-leaderboard.js
+++ plugins/rpg-leaderboard.js
@@ -21,23 +21,23 @@ const handler = async (m, {conn, args, participants}) => {
 ];
   const randomAdventurePhrase = adventurePhrases[Math.floor(Math.random() * adventurePhrases.length)];
   const texto = `
-🌐𝐓𝐎𝐃𝐎𝐒 𝐀 𝐏𝐎𝐑 𝐄𝐋 𝐏𝐎𝐃𝐄𝐑🌐
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-—◉ 𝗧𝗢𝗣𝗦 𝗥𝗔𝗡𝗚𝗢𝗦 (𝐓𝐔) ${len} 𝗘𝗫𝗣 [🌧️]
+🌐𝑇𝑜𝑑𝑜𝑠 𝐴 𝑃𝑜𝑟 𝑒𝑙 𝑃𝑜𝑑𝑒𝑟🌐
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+—◉ 𝕿𝖔𝖕𝖘 𝕽𝖆𝖓𝖌𝖔𝖘 (𝐓𝐔) ${len}𝕰𝖝𝖕𝖊𝖗𝖙𝖔 [🌧️]
 
-[💖]𝗧𝘂 𝗿𝗮𝗻𝗴𝗼 𝗲𝗻 𝗹𝗮 𝘁𝗮𝗯𝗹𝗮:✨ ${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}
+🌹𝑻𝒖 𝑹𝒂𝒏𝒈𝒐 𝒆𝒏 𝒍𝒂 𝑻𝒂𝒃𝒍𝒂:✨ ${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}
 
 ${sortedExp.slice(0, len).map(({jid, exp}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} exp*`).join`\n`}
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-—◉ 𝗧𝗢𝗣𝗦 𝗥𝗔𝗡𝗚𝗢𝗦 (𝐓𝐔) ${len} 𝗗𝗜𝗔𝗠𝗔𝗡𝗧𝗘𝗦 [☃️]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+—◉ 𝕿𝖔𝖕𝖘 𝕽𝖆𝖓𝖌𝖔𝖘 (𝐓𝐔) ${len}𝕯𝕴𝖆𝖒𝖆𝖓𝖙𝖊𝖘 [☃️]
 
-[💖]𝗧𝘂 𝗿𝗮𝗻𝗴𝗼 𝗲𝗻 𝗹𝗮 𝘁𝗮𝗯𝗹𝗮:✨ ${usersLim.indexOf(m.sender) + 1} de ${usersLim.length}
+🌹𝑻𝒖 𝑹𝒂𝒏𝒈𝒐 𝒆𝒏 𝒍𝒂 𝑻𝒂𝒃𝒍𝒂:✨ ${usersLim.indexOf(m.sender) + 1} de ${usersLim.length}
 
 ${sortedLim.slice(0, len).map(({jid, limit}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} diamantes*`).join`\n`}
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-—◉ 𝗧𝗢𝗣𝗦 𝗥𝗔𝗡𝗚𝗢𝗦 (𝐓𝐔) ${len} 𝗟𝗘𝗩𝗘𝗟 [☔]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+—◉ 𝕿𝖔𝖕𝖘 𝕽𝖓𝖌𝖔𝖘 (𝐓𝐔) ${len}𝕹𝖎𝖛𝖊𝖑 [☔]
 
-[💖]𝗧𝘂 𝗿𝗮𝗻𝗴𝗼 𝗲𝗻 𝗹𝗮 𝘁𝗮𝗯𝗹𝗮:✨ ${usersLevel.indexOf(m.sender) + 1} de ${usersLevel.length}
+🌹𝑻𝒖 𝑹𝒂𝒏𝒈𝒐 𝒆𝒏 𝒍𝒂 𝑻𝒂𝒃𝒍𝒂:✨ ${usersLevel.indexOf(m.sender) + 1} de ${usersLevel.length}
 
 ${sortedLevel.slice(0, len).map(({jid, level}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *nivel ${level}*`).join`\n`}
 
