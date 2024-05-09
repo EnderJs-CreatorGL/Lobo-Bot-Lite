943cff4f9d82efd00429383c6052bf1fc0f25dde
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-09 11:08:10 -0600

    Actualizar downloader-stickerpack.js

--- plugins/downloader-stickerpack.js
+++ plugins/downloader-stickerpack.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch';
 import {sticker} from '../lib/sticker.js';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}* https://getstickerpack.com/stickers/flork-memes-4-1`;
+  if (!text) throw `*[🌹] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}* https://getstickerpack.com/stickers/flork-memes-4-1`;
   try {
     const url = text;
     const res = await fetch(`https://api.akuari.my.id/downloader/stickerpack?link=${url}`);
@@ -13,7 +13,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
       // await delay(1500)
     }
   } catch {
-    await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
+    await m.reply('*[🌹] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
   }
 };
 handler.command = /^stickerpack$/i;
