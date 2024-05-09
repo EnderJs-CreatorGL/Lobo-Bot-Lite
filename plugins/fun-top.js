091878770f58028c2c6b878d6bbde429a4ca508c
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-09 11:20:19 -0600

    Actualizar fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -31,10 +31,7 @@ function handler(m, {groupMetadata, command, conn, text, usedPrefix}) {
 *9. ${user(i)}*
 *10. ${user(j)}*`;
   m.reply(top, null, {mentions: [a, b, c, d, e, f, g, h, i, j]});
-  conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
-    type: 'audioMessage',
-    ptt: true});
-}
+  
 handler.help = handler.command = ['top'];
 handler.tags = ['fun'];
 handler.group = true;
