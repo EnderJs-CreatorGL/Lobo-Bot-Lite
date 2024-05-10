3f5bfba0869fde2691339591ae6d66bce64c3013
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-05-09 22:26:32 -0600

    Actualizar santa-biblia.js

--- plugins/santa-biblia.js
+++ plugins/santa-biblia.js
@@ -1,11 +1,13 @@
 const handler = async (m, { conn, command, text }) => {
   const bibliaporcentaje = Math.floor(Math.random() * 100);
   const isHighbiblia = bibliaporcentaje >= 50;
-  const bibliaMessages = ["Isaias 41:10, No tengas miedo por que yo estoy con tigo, No te desalientes por que yo soy tu Dios, Te dare fuerzas y te ayudare te sostendre con mi mano derecha victoriosa";
+  const bibliaMessages = ["Isaias 41:10, No tengas miedo por que yo estoy con tigo, No te desalientes por que yo soy tu Dios, Te dare fuerzas y te ayudare te sostendre con mi mano derecha victoriosa",
 
   ];
-  const notSoHighbibliaMessages = ["Isaias 41:10, No tengas miedo por que yo estoy con tigo, No te desalientes por que yo soy tu Dios, Te dare fuerzas y te ayudare te sostendre con mi mano derecha victoriosa,;
 
+  const notSoHighbibliaMessages = ["Isaias 41:10, No tengas miedo por que yo estoy con tigo, No te desalientes por que yo soy tu Dios, Te dare fuerzas y te ayudare te sostendre con mi mano derecha victoriosa",
+
+];
   const bibliaDescription = isHighLove ? "Esta es la palabra de Dios";
 
 
