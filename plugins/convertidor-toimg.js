285c3954d76bbac0610004a25f3768852e37c855
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-08-08 21:58:28 -0600

    Actualizar convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -2,11 +2,17 @@ import { webp2png } from '../lib/webp2mp4.js'
 
 var handler = async (m, { conn, usedPrefix, command }) => {
 
-const notStickerMessage = `*⚠️ RESPONDA CON ${usedPrefix + command} AL STICKER QUE DESEE CONVERTIR EN IMAGEN*`
+const notStickerMessage = `🌹 *Responda a una imagen*`
 if (!m.quoted) throw notStickerMessage 
 const q = m.quoted || m
 let mime = q.mediaType || ''
 if (!/sticker/.test(mime)) throw notStickerMessage
+conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
 await conn.sendFile(m.chat, out, 'error.png', null, m, m)
@@ -16,6 +22,6 @@ handler.help = ['toimg']
 handler.tags = ['transformador']
 handler.command = ['toimg', 'jpg', 'jpge', 'png']
 
-handler.limit = true
+handler.estrellas = 2
 
 export default handler
