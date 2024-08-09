9097588cfede2b9611676159b0dfdbab5f724d39
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 21:28:01 -0500

    Actualizar di.js

--- plugins/di.js
+++ plugins/di.js
@@ -5,22 +5,19 @@
 
 let handler = async(m, { conn, text, command, usedPrefix }) => {
 
-//envia para que escriba el nombre de las 2 personas, no pongas esto: .amorverdadero Alejandro y Maria pon solo: .amorverdadero Alejandro Maria
+//Envia para que escriba el nombre de las 2 personas, no pongas esto: .amorverdadero Alejandro y Maria pon solo: .amorverdadero Alejandro Maria
 if (!text) return conn.reply(m.chat, `⚜️ Te faltó el nombre de 2 personas...\n🚩 Ejemplo: ${usedPrefix + command} Alejandro Maria`, m, fake, )
 let [text1, ...text2] = text.split(' ')
 
-//son 2 nombres..
+//Son 2 nombres..
 text2 = (text2 || []).join(' ')
 if (!text2) return conn.reply(m.chat, `⚜️ Son 2 nombres por favor...\n🚩 Ejemplo: ${usedPrefix + command} Alejandro Maria`, m, fake, )
 
-//text de donde viene.
+//Text de donde envia el mensaje.
 let amor = `*${text1} Busca A: ${text2} Para Que L@ Mantenga* 🥺`
 
-//Enviar el mensaje!
+//Envia el mensaje!
 conn.reply(m.chat, amor, m, fake, )
-
-//sirve por si algo ponen "@", para que se pueda ver la mention!, esta funcion está desactivada con "//", por si algo ponle "//" al de arriba y el de abajo le quitas lo "//"
-//m.reply(amor, null, { mentions: conn.parseMention(amor) })
 
 }
 handler.help = ['amorverdadero']
