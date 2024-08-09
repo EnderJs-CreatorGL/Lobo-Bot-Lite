6e0ff8c2ef01d8d19b482f7c2718a15c03da9b31
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 20:55:27 -0500

    Actualizar bot-allfake.js

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -28,20 +28,21 @@ console.log(`Error : ${e}`)
 }}
 
 let pp = ''
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+//let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/1861aab98389b13db8588.jpg')
 
-//tags
-global.nombre = conn.getName(m.sender)
-global.taguser = '@' + m.sender.split("@s.whatsapp.net")
-var more = String.fromCharCode(8206)
-global.readMore = more.repeat(850)
+/* 
+Mensajes Fakes
+*/
 
-//Fakes
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-global.fake = rcanal
+global.nombre = conn.getName(m.sender)
+global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363193060502597@newsletter", serverMessageId: 100, newsletterName: '😊 Lobo Bot - Lite', }, externalAdReply: { showAdAttribution: true, title: packname, body: wm, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: imagen1, sourceUrl: md, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = fake
 
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363193060502597@newsletter', newsletterName: "Lobo Bot, By: Ender", serverMessageId: -1 }
+}}, { quoted: m }
+}
 export default handler
