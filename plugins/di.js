a8076ea3aed7e27968eb1fb05851f46c5cb33c71
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 21:17:02 -0500

    Actualizar di.js

--- plugins/di.js
+++ plugins/di.js
@@ -3,13 +3,17 @@
 • OfcDiego (https://github.com/OfcDiego)
 */
 
-let handler = async(m, { conn }) => {
+let handler = async(m, { conn, text }) => {
 
 let user = conn.getName(who)
 
-//const img = './media/amorverdadero.js'
+if (!text) return conn.reply(m.chat, '⚜️ Te faltó el nombre...', m, fake, )
+let [text1, ...text2] = text.split(' ')
 
-let amor = `*${user} Busca Una Pareja Que Lo/a Mantenga 💞*`
+text2 = (text2 || []).join(' ')
+if (!text2) return conn.reply(m.chat, `⚜️ Son 2 nombres por favor...`, m, fake, )
+
+let amor = `*${text1} Busca A: ${text2} Para Que L@ Mantenga 💞*`
 
 conn.reply(m.chat, amor, m, fake, )
 
