b4a73f786b4678ce90b1ef9cee0f5ebb9f9198d2
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 20:36:38 -0500

    Actualizar random-cat.js

--- plugins/random-cat.js
+++ plugins/random-cat.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
-const handler = async (m, {conn, text}) => {
+const handler = async (m, {conn, command, text}) => {
   try {
     const res = await fetch('https://api.thecatapi.com/v1/images/search');
     const img = await res.json();
     const caption = `
-_©The Mystic - Bot_
+_${command}_
 `.trim();
     conn.sendFile(m.chat, img[0].url, 'cat.jpg', caption, m);
   } catch (e) {
