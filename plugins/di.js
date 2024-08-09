2794143f726c5e3316d8e6ff6eaf2f08281e75eb
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 21:12:52 -0500

    ⚜️ N A M A S - E S - E L - C O M I E N Z O !!

--- plugins/di.js
+++ plugins/di.js
@@ -1,25 +1,21 @@
-//Comando por: diegojadibot
+/*
+🍧 Codigo Realizado Por : 
+• OfcDiego (https://github.com/OfcDiego)
+*/
 
-/*let handler = async(m, { conn, text, usedPrefix, command }) => {
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = conn.getName(who)
-            const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-//const pp = './storage/logos/Menu2.jpg'
+let user = conn.getName(who)
 
-//let di = `> Texto: ${text} 🏴\n\n*${name} quiere yaoi 💋*`
+//const img = './media/amorverdadero.js'
 
-conn.reply(m.chat, `> Nota: ${text} ☁️\n\n*${name} Busca Novio/a 🌼*`, m, fake, )
+let amor = `*${user} Busca Una Pareja Que L@ Mantenga 💞*`
 
-conn.reply(m.chat, `*Buscando el amor para: ${name}*`, m, fake, )
-
-conn.reply(m.chat, `*${name} No Ecuentra El Amor*`, m, fake, )
-
-//conn.reply(m.chat, `*Buscando el amor para: ${name}*`, m, fake, )
+conn.reply(m.chat, amor, m, fake, )
 
 }
-handler.help = ['di']
-handler.tags = ['decir']
-handler.command = /^(decir|di|buscar-novio|buscar-novia|buscarnovio|buscarnovia)$/i
+handler.help = ['amorverdadero']
+handler.tags = ['fun']
+handler.command = ['buscarnovia', 'buscarnovio', 'buscarnv', encontraramor', 'econtrarelamor', 'amorverdadero']
 handler.register = true
-export default handler*/
+export default handler
