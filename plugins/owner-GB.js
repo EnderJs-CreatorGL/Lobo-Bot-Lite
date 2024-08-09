843496d18eb357eac4e4ad286bcc8bdafb08e762
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 20:35:49 -0500

    Actualizar owner-GB.js

--- plugins/owner-GB.js
+++ plugins/owner-GB.js
@@ -1,4 +1,4 @@
-let handler = (m) => m;
+/*let handler = (m) => m;
 handler.all = async function (m) {
   
 
@@ -15,4 +15,4 @@ handler.all = async function (m) {
   }
   return !0;
 };
-export default handler;
+export default handler;*/
