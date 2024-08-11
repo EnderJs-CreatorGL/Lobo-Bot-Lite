4180f8ee029a106added40b4bc25b00f9038e5ee
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-08-10 23:08:04 -0600

    Actualizar random-wallprandoms.js

--- plugins/random-wallprandoms.js
+++ plugins/random-wallprandoms.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
 const handler = async (m, {command, conn}) => {
   const apikey = global.keysxxx;
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
