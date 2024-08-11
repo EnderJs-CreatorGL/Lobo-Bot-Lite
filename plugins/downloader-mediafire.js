14f20162072c808dd826125c7d51a6626540faf3
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-08-10 23:29:20 -0600

    Actualizar downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -40,7 +40,6 @@ export default handler;
 
 async function mediafireDl(url) {
   const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/', '')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`);
-  const $ = cheerio.load(res.data);
   const link = $('#downloadButton').attr('href');
   const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ', '').replaceAll('\n', '');
   const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text();
