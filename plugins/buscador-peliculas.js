5de60f5c3a314096cf3e3cd7c8990a89013cc23d
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-08-10 23:04:35 -0600

    Actualizar buscador-peliculas.js

--- plugins/buscador-peliculas.js
+++ plugins/buscador-peliculas.js
@@ -2,7 +2,6 @@
 
 import fetch from 'node-fetch';
 import axios from 'axios';
-import {load} from 'cheerio';
 const handler = async (m, {text, usedPrefix, command, conn}) => {
   if (!text) throw '[❗]𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐥𝐚 𝐩𝐞𝐥𝐢 𝐚 𝐛𝐮𝐬𝐜𝐚𝐫';
   let aaaa;
