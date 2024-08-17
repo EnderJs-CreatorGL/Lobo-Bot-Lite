cd04f1b9a3f9f6ead6c785431f7fc2c4a418c813
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-17 12:23:25 -0500

    Actualizar plugins

---
+++ plugins/main-host.js
@@ -0,0 +1,42 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let txt = `🚩 *O L Y M P U S - H O S T* 
+
+*¿Quieres un Host de calidad y con bajos precios?*
+Pues te presento a *OlympusHost*, un hosting de calidad con servidores dedicados y precios por debajo de 1USD, estos servidores están destinados a ofrecerte un Uptime 24/7 para que puedas alojar tus proyectos y qué estos funcionen de manera eficaz.
+
+🟢 \`\`\`Información del Host\`\`\`
+
+🔮 *Dashboard:* 
+• ${dash}
+
+🧃 *Panel:*
+• ${panel}
+
+💥 *Canal:*
+• ${channel}
+
+🍭 *Grupo Oficial:*
+• ${grupohost}
+
+⚜️ *Contacto (Mateo)*
+https://wa.me/593979133620
+
+> *Únete a está comunidad y disfruta de un servicio de calidad :D*` 
+await conn.sendMessage(m.chat, { text: txt,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `🔵 O L Y M P U S - H O S T 🔵`,
+body: `⚜️ Super Hosting 24/7 ⚜️`,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://qu.ax/Eqii.jpg', 
+sourceUrl: 'https://dash.olympus-host.xyz'}}},
+{ quoted: m})
+}
+handler.tags = ['main'] 
+handler.help = ['host', 'hosting'] 
+handler.command = ['host', 'olympus', 'olympushost', 'hosting']
+export default handler
