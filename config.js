bd1dd5de8d81fd1ec5c2076f9c18a9f0a4d42d10
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-01-14 08:49:23 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,39 +1,85 @@
 import {watchFile, unwatchFile} from 'fs';
 import chalk from 'chalk';
 import {fileURLToPath} from 'url';
-import fs from 'fs';
-import * as cheerio from 'cheerio';
-import fetch from 'node-fetch';
-import axios from 'axios';
+import fs from 'fs'; 
 import moment from 'moment-timezone';
-import { es } from "./lib/idiomas/total-idiomas.js"
 
-global.owner = [ 
-['50558124470', '𝐂𝐫𝐞𝐚𝐝𝐨𝐫 🌹', true],
-['593939005387'],
-['573012482597']]
+global.botnumber = ""
+global.confirmCode = ""
+global.authFile = `MysticSession`;
 
-global.suittag = ['50558124470']
-global.prems = []
+// Cambiar a true si el Bot responde a sus comandos con otros comandos.
+// Cambiar a false para usar el Bot desde el mismo numero del Bot.
+// Error de m.isBaileys marcado como false fix temporal
+global.isBaileysFail = false
 
-global.lenguajeLB = es //idioma de lobobot
+global.defaultLenguaje = 'es';
 
-global.packname = 'Lobo Bot Lite 😍';
-global.author = 'By Ender 🎭';
-global.wm = 'Lobo-Bot-Lite';
-global.titulowm = 'Lobo-Bot-Lite';
-global.titulowm2 = `By Ender`
-global.igfg = 'By Ender'  ;
-global.wait = '[🔮] ᴇᴊᴇᴄᴜᴛᴀɴᴅᴏ ᴇsᴘᴇʀᴇ ...';
-global.vs = 'V2';
+global.owner = [
+  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
+  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
+  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
+  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
+  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
+  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
+  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
+  ['573012482597', '💫 Mystic - OfcKing', true],
+  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
+  ['5493794297363', '💫 Mystic - GabrielVz', true], 
+  ['393518398856', '💫 Mystic - Tester Fabri115💫', true],
+  ['212781160203', '💫 Mystic - Saad 💫', true], 
+  ['5217778573223', '💫 Mystic - Sarethh 💫', true],
+  ['593968585283'],
+  ['5492266466080'],
+  ['5218442114446'],
+  ['59894808483'],
+  ['593980586516'], 
+  ['595975740803'],  
+  ['5492266613038'],
+  ['50497150165'],
+  ['51906662557'],
+  ['573183650526'], 
+  ['5217441298510'], 
+  ['5217294888993'],
+  ['595992611272'],
+  ['5219992843892'],
+  ['5493795319022'],
+  ['573147616444'], 
+  ['5219991402134'],
+  ['5219996125657'],
+  ['5492215034412'],
+  ['584125778026'],
+  ['5218444937407'],
+  ['5218713339906']
+];
 
-global.imagen1 = fs.readFileSync('./Menu2.jpg');
-global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
-global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
-global.imagen4 = fs.readFileSync('./Menu.png');
-global.imagen5 = fs.readFileSync('./src/+18.jpg');
-global.imagen6= fs.readFileSync('./Menu3.png');
-global.imagen7= fs.readFileSync('./src/hjbot.jpg');
+global.suittag = ['5219993404349'];
+global.prems = ['51995386439'];
+
+// API
+global.BASE_API_DELIRIUS = "https://deliriussapi-oficial.vercel.app";
+
+global.packname = 'Sticker';
+global.author = 'The Mystic - Bot';
+global.wm = 'The Mystic - Bot';
+global.titulowm = 'The Mystic - Bot';
+global.titulowm2 = `The Mystic - Bot`
+global.igfg = 'The Mystic - Bot';
+global.wait = '*_[ ⏳ ] Cargando..._*';
+
+/*global.imagen1 = fs.createReadStream('./src/assets/images/menu/languages/es/menu.png');
+global.imagen2 = fs.createReadStream('./src/assets/images/menu/languages/pt/menu.png');
+global.imagen3 = fs.createReadStream('./src/assets/images/menu/languages/fr/menu.png');
+global.imagen4 = fs.createReadStream('./src/assets/images/menu/languages/en/menu.png');
+global.imagen5 = fs.createReadStream('./src/assets/images/menu/languages/ru/menu.png');*/
+
+global.imagen1 = fs.readFileSync('./src/assets/images/menu/languages/es/menu.png');
+global.imagen2 = fs.readFileSync('./src/assets/images/menu/languages/pt/menu.png');
+global.imagen3 = fs.readFileSync('./src/assets/images/menu/languages/fr/menu.png')
+global.imagen4 = fs.readFileSync('./src/assets/images/menu/languages/en/menu.png')
+global.imagen5 = fs.readFileSync('./src/assets/images/menu/languages/ru/menu.png')
+
 global.mods = [];
 
 //* *******Tiempo***************
@@ -44,28 +90,22 @@ global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', yea
 global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
-
-var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
-global.saludo = "" + hour;
-
-//****************************
-global.gp1 = 'https://whatsapp.com/channel/0029VaCtl9r05MUjcuMd9l2W'
-global.gp2 = 'https://chat.whatsapp.com/KrN4WR14z282sQPpRPdqLu'
-global.channel2 = 'https://whatsapp.com/channel/0029VaCtl9r05MUjcuMd9l2W'
-global.wm2 = `▸ ${dia} ${fecha}\n▸ Lobo`;
-global.gt = 'Lobo Bot';
-global.Lobo = 'Lobo Bot';
-global.md = 'https://github.com/EnderJs-CreatorGL/Lobo-Bot-Lite';
-global.Lobo = 'https://github.com/EnderJs-CreatorGL/Lobo-Bot-Lite';
-global.waitt = '[⛔] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...';
-global.waittt = '[⛔] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...';
-global.waitttt = '[⛔] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...';
-global.nomorown = '50558124470';
+//* ****************************
+global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
+global.gt = 'The Mystic - Bot';
+global.mysticbot = 'The Mystic - Bot';
+global.channel = 'https://whatsapp.com/channel/0029Vaein6eInlqIsCXpDs3y';
+global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
+global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
+global.waitt = '*_[ ⏳ ] Cargando..._*';
+global.waittt = '*_[ ⏳ ] Cargando..._*';
+global.waitttt = '*_[ ⏳ ] Cargando..._*';
+global.nomorown = '5219993404349';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
 global.cmenuf = '╰━═┅═━––––––๑\n';
-global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
 global.dmenut = '*❖─┅──┅〈*';
 global.dmenub = '*┊»*';
 global.dmenub2 = '*┊*';
@@ -75,9 +115,9 @@ global.htki = '*⭑•̩̩͙⊱•••• ☪*';
 global.htka = '*☪ ••••̩̩͙⊰•⭑*';
 global.comienzo = '• • ◕◕════';
 global.fin = '════◕◕ • •';
-global.botdate = `☆ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}`; // Asia/Jakarta
-global.bottime = `[🗒️] 🅳︎🅰︎🆃︎🅴︎: ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`;// America/Los_Angeles
-global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.botdate = `*[ 📅 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
+global.bottime = `*[ ⏳ ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
+global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./src/assets/images/menu/languages/es/menu.png')}}};
 global.multiplier = 99;
 global.flaaa = [
   'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
@@ -87,7 +127,8 @@ global.flaaa = [
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
 ];
 //* ************************
-let file = fileURLToPath(import.meta.url);
+
+const file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
   unwatchFile(file);
   console.log(chalk.redBright('Update \'config.js\''));
