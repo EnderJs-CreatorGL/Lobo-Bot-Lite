43415f5cf74083b881347e21c8f40772beb6b51b
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-01-13 22:45:24 -0600

    Actualizar buscador-wikipedia.js

--- plugins/buscador-wikipedia.js
+++ plugins/buscador-wikipedia.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import fetch from 'node-fetch';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 async function wikipedia(querry) {
   try {
     const link = await axios.get(`https://es.wikipedia.org/wiki/${querry}`);
