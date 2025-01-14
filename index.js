3ac9270769a652990b18011a449319b40906f6ce
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-01-13 23:18:57 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -10,88 +10,67 @@ import yargs from 'yargs'
 
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
-const { name, author } = require(join(__dirname, './package.json'))
+const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
+const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
 say('LoboBotLite', {
-  font: 'block',
-  align: 'center',
-  colors: ['cyan', 'magenta']
+font: 'block',
+align: 'center',
+colors: ['cyan', 'magenta']
 })
 say(`By: Ender`, {
-  font: 'console',
-  gradient: ['blue', 'green']
+font: 'console',
+gradient: ['blue', 'green']
 })
 
 var isRunning = false
-
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
 function start(file) {
-  if (isRunning) return
-  isRunning = true
-  let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['blue', 'magenta']
-  })
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  })
-  let p = fork()
-  p.on('message', data => {
-    switch (data) {
-      case 'reset':
-        p.process.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
-      case 'uptime':
-        p.send(process.uptime())
-        break
-    }
-  })
-  p.on('exit', (_, code) => {
-    isRunning = false
-    console.error('⚠️ Ocurrió un error inesperado:', code)
-    process.exit();
-    if (code === 0) return
-    watchFile(args[0], () => {
-      unwatchFile(args[0])
-      start(file)
-    })
-  })
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-  if (!opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
+if (isRunning) return
+isRunning = true
+let args = [join(__dirname, file), ...process.argv.slice(2)]
+say([process.argv[0], ...args].join(' '), {
+font: 'console',
+align: 'center',
+gradient: ['blue', 'magenta']
+})
+setupMaster({
+exec: args[0],
+args: args.slice(1),
+})
+let p = fork()
+p.on('message', data => {
+switch (data) {
+case 'reset':
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+break
+case 'uptime':
+p.send(process.uptime())
+break
+}
+})
+p.on('exit', (_, code) => {
+isRunning = false
+console.error('⚠️ Ocurrió un error inesperado:', code)
+process.exit();
+if (code === 0) return
+watchFile(args[0], () => {
+unwatchFile(args[0])
+start(file)
+})
+})
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test'])
+if (!rl.listenerCount()) rl.on('line', line => {
+p.emit('message', line.trim())
+})
 }
 
-async function main() {
-  const opcion = await new Promise((resolve) => {
-    rl.question('—◉ㅤSeleccione una opción (solo el numero):
-1. Con código QR
-2. Con código de texto de 8 dígitos
-—> ', resolve);
-  });
-
-  if (opcion === '1') {
-    process.argv.push('qr');
-  } else if (opcion === '2') {
-    const codigoOchoDigitos = await new Promise((resolve) => {
-      rl.question('—◉ㅤIngrese su código de 8 dígitos:
-—> ', resolve);
-    });
-    process.argv.push(codigoOchoDigitos);
-  } else {
-    console.log('Opción no válida. Saliendo...');
-    process.exit(0);
-  }
-
-  start('main.js');
-}
-
-main();
+start('main.js')
